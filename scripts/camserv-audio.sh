796515c25efe0e71475c6561cbbb76490b63635a
Thomas Vander Stichele <thomas@apestaart.org>
2004-06-04 18:34:33 +0000

    changes

--- scripts/camserv-audio.sh
+++ scripts/camserv-audio.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
+export FLUPATH=`pwd`/../flumotion
 
 echo Starting controller
-python controller.py  > /tmp/flumotion.controller.log 2>&1 &
+python $FLUPATH/controller.py  > /tmp/flumotion.controller.log 2>&1 &
 sleep 3
 xterm -T controller -e sh -c "tail -f /tmp/flumotion.controller.log" &
 
 echo Starting producer for video
-python producer.py -v -c localhost -n producer-video -p "v4lsrc device=/dev/video0 ! video/x-raw-yuv,width=320,height=240"  > /tmp/flumotion.producer-video.log 2>&1 &
+python $FLUPATH/producer.py -v -c localhost -n producer-video -p "v4lsrc device=/dev/video0 ! video/x-raw-yuv,width=320,height=240"  > /tmp/flumotion.producer-video.log 2>&1 &
 sleep 3
 xterm -T producer-video -e sh -c "tail -f /tmp/flumotion.producer-video.log" &
 
 echo Starting producer for audio
-python producer.py -v -c localhost -n producer-audio -p "alsasrc period-size=500 period-count=10 device=hw:1 ! audio/x-raw-int,rate=8000,signed=(boolean)true,endianness=1234,channels=1,width=16,depth=16"  > /tmp/flumotion.producer-audio.log 2>&1 &
+python $FLUPATH/producer.py -v -c localhost -n producer-audio -p "alsasrc period-size=500 period-count=10 device=hw:1 ! audio/x-raw-int,rate=8000,signed=(boolean)true,endianness=1234,channels=1,width=16,depth=16"  > /tmp/flumotion.producer-audio.log 2>&1 &
 sleep 3
 xterm -T producer-audio -e sh -c "tail -f /tmp/flumotion.producer-audio.log" &
 
 echo Starting converter
-python converter.py -v -c localhost -s producer-audio,producer-video -n converter -p "{ @producer-audio ! mulawenc name=aenc } { @producer-video ! jpegenc quality=30 name=venc } aenc. ! queue max-size-buffers=2 ! multipartmux name=mux venc. ! queue max-size-buffers=2 ! mux. mux."  > /tmp/flumotion.converter.log 2>&1 &
+python $FLUPATH/converter.py -v -c localhost -s producer-audio,producer-video -n converter -p "{ @producer-audio ! mulawenc name=aenc } { @producer-video ! jpegenc quality=30 name=venc } aenc. ! queue max-size-buffers=4 ! multipartmux name=mux venc. ! queue max-size-buffers=4 ! mux. mux."  > /tmp/flumotion.converter.log 2>&1 &
 sleep 3
 xterm -T converter -e sh -c "tail -f /tmp/flumotion.converter.log" &
 
 echo Starting streamer
-python streamer.py -v -c localhost -s converter -n streamer -p http -o 8080  > /tmp/flumotion.streamer.log 2>&1 &
+python $FLUPATH/streamer.py -v -c localhost -s converter -n streamer -p http -o 8080  > /tmp/flumotion.streamer.log 2>&1 &
 sleep 3
 xterm -T streamer -e sh -c "tail -f /tmp/flumotion.streamer.log" &
 
--- scripts/camserv.sh
+++ scripts/camserv.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
+export FLUPATH=`pwd`/../flumotion
 
 echo Starting controller
-python controller.py  > /tmp/flumotion.controller.log 2>&1 &
+python $FLUPATH/controller.py  > /tmp/flumotion.controller.log 2>&1 &
 sleep 3
 xterm -T controller -e sh -c "tail -n 10000 -f /tmp/flumotion.controller.log" &
 
 echo Starting producer
-python producer.py -v -c localhost -n producer -p "v4lsrc device=/dev/video0 ! video/x-raw-yuv,width=320,height=240"  > /tmp/flumotion.producer.log 2>&1 &
+python $FLUPATH/producer.py -v -c localhost -n producer -p "v4lsrc device=/dev/video0 ! video/x-raw-yuv,width=320,height=240"  > /tmp/flumotion.producer.log 2>&1 &
 sleep 3
 xterm -T producer -e sh -c "tail -n 10000 -f /tmp/flumotion.producer.log" &
 
 echo Starting converter
-python converter.py -v -c localhost -s producer -n converter -p "ffmpegcolorspace ! jpegenc ! multipartmux"  > /tmp/flumotion.converter.log 2>&1 &
+python $FLUPATH/converter.py -v -c localhost -s producer -n converter -p "ffmpegcolorspace ! jpegenc ! multipartmux"  > /tmp/flumotion.converter.log 2>&1 &
 sleep 3
 xterm -T converter -e sh -c "tail -n 10000 -f /tmp/flumotion.converter.log" &
 
 echo Starting streamer
-python streamer.py -v -c localhost -s converter -n streamer -p http -o 8080  > /tmp/flumotion.streamer.log 2>&1 &
+python $FLUPATH/streamer.py -v -c localhost -s converter -n streamer -p http -o 8080  > /tmp/flumotion.streamer.log 2>&1 &
 sleep 3
 xterm -T streamer -e sh -c "tail -n 10000 -f /tmp/flumotion.streamer.log" &
 
