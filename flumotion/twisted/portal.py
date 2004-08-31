9f96c8b0bbe57cb04ac40f6345e667e848daf102
Thomas Vander Stichele <thomas@apestaart.org>
2004-08-31 10:46:10 +0000

    use custom namedAny that matches string for ImportError

--- flumotion/twisted/portal.py
+++ flumotion/twisted/portal.py
@@ -2,9 +2,41 @@ import md5
 
 from twisted.cred.portal import IRealm, Portal
 from twisted.python.components import registerAdapter
-from twisted.python.reflect import namedAny
 from twisted.spread import pb
 from twisted.spread import flavors
+
+### stolen from twisted.python.reflect and changed
+### the version in Twisted 1.3.0 checks length of backtrace as metric for
+### ImportError; for me this fails because two lines of ihooks.py are in
+### between
+### filed as http://www.twistedmatrix.com/users/roundup.twistd/twisted/issue698
+### remove this when fixed and depending on new upstream twisted
+def namedAny(name):
+    """Get a fully named package, module, module-global object, or attribute.
+    """
+    names = name.split('.')
+    topLevelPackage = None
+    moduleNames = names[:]
+    while not topLevelPackage:
+        try:
+            trialname = '.'.join(moduleNames)
+            topLevelPackage = __import__(trialname)
+        except ImportError:
+            import sys
+            # if the ImportError happened in the module being imported,
+            # this is a failure that should be handed to our caller.
+
+            if str(sys.exc_info()[1]) != "No module named %s" % trialname:
+                raise
+            moduleNames.pop()
+                                                                                
+    obj = topLevelPackage
+    for n in names[1:]:
+        obj = getattr(obj, n)
+                                                                                
+    return obj
+
+
 
 class FlumotionPortal(Portal):
     pass
@@ -32,6 +64,7 @@ class _PortalWrapper(pb.Referenceable):
     def remote_login(self, username, *interfaces):
         """Start of username/password login."""
         interfaces = [namedAny(interface) for interface in interfaces]
+	print "DEBUG: interfaces:", interfaces
         c = pb.challenge()
         return c, _PortalAuthChallenger(self, username, c, *interfaces)
 
