da75efdc215a0296ad114bccf5e2643e72b97fc2
Thomas Vander Stichele <thomas@apestaart.org>
2004-09-08 11:39:19 +0000

    fixed make distcheck

--- testsuite/runtests.py
+++ testsuite/runtests.py
@@ -4,19 +4,13 @@ import os
 import sys
 import unittest
 
-SKIP_FILES = ['common', 'runtests', 'webbie']
-
-
-dir = os.path.join(os.path.split(os.path.abspath(__file__))[0], 'tests')
+# testsuite srcdir
+srcdir = os.path.join(os.path.split(os.path.abspath(__file__))[0], 'tests')
 
 def gettestnames(dir):
     files = glob.glob(os.path.join(dir, '*.py'))
     fullnames = map(lambda x: x[:-3], files)
     names = map(lambda x: os.path.split(x)[1], fullnames)
-    for f in SKIP_FILES:
-        if f in names:
-             names.remove(f)
-    print names
     return names
         
 suite = unittest.TestSuite()
@@ -28,14 +22,7 @@ try:
 except:
     pass
 
-names = gettestnames(dir)
-
-# make sure we're in the subdirectory where tests/*.py lives
-if os.environ.has_key('TESTSDIR'):
-    os.chdir(os.environ['TESTSDIR'])
-    sys.path.insert(0, os.environ['TESTSDIR'])
-else:
-    os.chdir('tests')
+names = gettestnames(srcdir)
 
 for name in names:
     print os.getcwd()
