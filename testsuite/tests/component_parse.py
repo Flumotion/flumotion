744b1bd6f3ecb76508157babb3c729ce0fadf941
Thomas Vander Stichele <thomas@apestaart.org>
2004-09-08 11:00:43 +0000

    move to testsuite

---
+++ testsuite/tests/component_parse.py
@@ -0,0 +1,64 @@
+import common
+import unittest
+
+from flumotion.component.component import ParseLaunchComponent
+
+class PipelineTest(ParseLaunchComponent):
+    def __init__(self, eaters, feeders):
+        self.__gobject_init__()
+        self.component_name = '<fake>'
+        self.eaters = eaters
+        self.feeders = feeders
+        self.remote = None
+        
+def pipelineFactory(pipeline, eaters=[], feeders=[]):
+    p = PipelineTest(eaters, feeders)
+    return p.parse_pipeline(pipeline)
+
+EATER = ParseLaunchComponent.EATER_TMPL
+FEEDER = ParseLaunchComponent.FEEDER_TMPL
+
+class TestParser(unittest.TestCase):
+    def testSimple(self):
+        assert pipelineFactory('foobar') == 'foobar'
+        assert pipelineFactory('foo ! bar') == 'foo ! bar'
+
+    def testOneSource(self):
+        res = pipelineFactory('@foo ! bar', ['foo'])
+        assert res == '%s name=foo ! bar' % EATER, res
+
+    def testOneSourceWithout(self):
+        res = pipelineFactory('bar', ['foo'])
+        assert res == '%s name=foo ! bar' % EATER, res
+
+    def testOneFeed(self):
+        res = pipelineFactory('foo ! :bar', [], ['bar'])
+        assert res == 'foo ! %s name=bar' % FEEDER, res
+        
+    def testOneFeedWithout(self):
+        res = pipelineFactory('foo', [], ['bar'])
+        assert res == 'foo ! %s name=bar' % FEEDER, res
+
+    def testTwoSources(self):
+        res = pipelineFactory('@foo ! @bar ! baz', ['foo', 'bar'])
+        assert res == '%s name=foo ! %s name=bar ! baz' \
+               % (EATER, EATER), res
+
+    def testTwoFeeds(self):
+        res = pipelineFactory('foo ! :bar ! :baz', [], ['bar', 'baz'])
+        assert res == 'foo ! %s name=bar ! %s name=baz' \
+               % (FEEDER, FEEDER), res
+
+    def testTwoBoth(self):
+        res = pipelineFactory('@eater1 ! @eater2 ! :feeder1 ! :feeder2',
+                              ['eater1', 'eater2',],
+                              ['feeder1', 'feeder2'])
+        assert res == ('%s name=eater1 ! %s name=eater2 ! ' % \
+                       (EATER, EATER) + 
+                       '%s name=feeder1 ! %s name=feeder2' % \
+                       (FEEDER, FEEDER))
+    def testErrors(self):
+        self.assertRaises(TypeError, pipelineFactory, '')
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ testsuite/tests/manager.py
@@ -0,0 +1,103 @@
+from common import unittest
+
+from flumotion.manager import component, manager
+
+class FakeComponentPerspective:
+    def __init__(self, name='fake', eaters=[], port=-1, listen_host='listen-host'):
+        self.name = name
+        self.eaters = eaters
+        self.port = port
+        self.listen_host = listen_host
+        
+    def getFeeders(self, long):
+        if long:
+            return [self.name + ':default']
+        else:
+            return ['default']
+
+    def getEaters(self):
+        return self.eaters
+    
+    def getListenHost(self):
+        return self.listen_host
+
+    def getListenPort(self, *args):
+        return self.port
+    
+    def getTransportPeer(self):
+        return 0, '127.0.0.1', 0
+
+    def getName(self):
+        return self.name
+
+class TestManager(unittest.TestCase):
+    def setUp(self):
+        self.manager = manager.Manager()
+
+    def testGetPerspective(self):
+        p = self.manager.getAvatar('foo-bar-baz')
+        assert isinstance(p, component.ComponentAvatar)
+
+        #self.assertRaises(AssertionError,
+        #                  self.manager.getPerspective, 'does-not-exist')
+
+    def testIsLocalComponent(self):
+        c = FakeComponentPerspective()
+        self.manager.addComponent(c)
+        assert self.manager.isLocalComponent(c)
+        
+    def testIsStarted(self):
+        c = self.manager.getAvatar('prod')
+        assert not self.manager.isComponentStarted('prod')
+        c.started = True # XXX: Use manager.componentStart
+        assert self.manager.isComponentStarted('prod')
+
+    def testGetComponent(self):
+        c = self.manager.getAvatar('prod')
+        assert self.manager.getComponent('prod') == c
+
+    def testHasComponent(self):
+        c = self.manager.getAvatar('prod')
+        assert self.manager.hasComponent('prod')
+        self.manager.removeComponent(c)
+        assert not self.manager.hasComponent('prod')
+        self.assertRaises(KeyError, self.manager.removeComponent, c)
+        
+    def testAddComponent(self):
+        c = FakeComponentPerspective('fake')
+        self.manager.addComponent(c)
+        assert self.manager.hasComponent('fake')
+        self.assertRaises(KeyError, self.manager.addComponent, c)
+        
+    def testRemoveComponent(self):
+        assert not self.manager.hasComponent('fake')
+        c = FakeComponentPerspective('fake')
+        self.assertRaises(KeyError, self.manager.removeComponent, c)
+        self.manager.addComponent(c)
+        assert self.manager.hasComponent('fake')
+        self.manager.removeComponent(c)
+        assert not self.manager.hasComponent('fake')
+        self.assertRaises(KeyError, self.manager.removeComponent, c)
+
+    def testComponentEatersEmpty(self):
+        c = FakeComponentPerspective('fake')
+        self.manager.addComponent(c)
+        assert self.manager.getComponentEaters(c) == []
+        
+    def testComponentsEaters(self):
+        c = FakeComponentPerspective('foo', ['bar', 'baz'])
+        self.manager.addComponent(c)
+        c2 = FakeComponentPerspective('bar', port=1000, listen_host='bar-host')
+        self.manager.addComponent(c2)
+        c3 = FakeComponentPerspective('baz', port=1001, listen_host='baz-host')
+        self.manager.addComponent(c3)
+        self.manager.feeder_set.addFeeders(c2)
+        self.manager.feeder_set.addFeeders(c3)
+        
+        eaters = self.manager.getComponentEaters(c)
+        assert len(eaters) == 2
+        assert ('bar', 'bar-host', 1000) in eaters
+        assert ('baz', 'baz-host', 1001) in eaters        
+
+if __name__ == '__main__':
+     unittest.main()
