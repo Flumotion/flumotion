0b212a38a2913584a1a263f3c13199627893e870
Johan Dahlin <johan@flumotion.com>
2004-09-16 14:30:07 +0000

    Add license header

--- flumotion/worker/__init__.py
+++ flumotion/worker/__init__.py
@@ -1,10 +1,6 @@
 # -*- Mode: Python -*-
-# vi:si:et:sw=4:sts=4:ts=4
-
 # Flumotion - a video streaming server
 # Copyright (C) 2004 Fluendo
-#
-# worker/worker.py: client-side objects to handle launching of components
 # 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,4 +15,3 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Street #330, Boston, MA 02111-1307, USA.
-
