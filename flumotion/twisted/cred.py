11d683f0a1fba8b463d4d6e28ac3e37c47ce8009
Johan Dahlin <johan@flumotion.com>
2004-09-16 16:31:28 +0000

    import error, so we can use this module similar to the real one in twisted

--- flumotion/twisted/cred.py
+++ flumotion/twisted/cred.py
@@ -20,7 +20,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Street #330, Boston, MA 02111-1307, USA.
 
-from twisted.cred import checkers, credentials
+from twisted.cred import checkers, credentials, error
 
 parent = checkers.InMemoryUsernamePasswordDatabaseDontUse
 class FlexibleCredentials(parent):
