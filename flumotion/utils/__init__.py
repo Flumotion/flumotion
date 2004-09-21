a4bf4ef6f7dd60203ba78a7b6381582f439d0b49
Johan Dahlin <johan@flumotion.com>
2004-09-21 09:47:26 +0000

    Move reload to reload.reload

--- flumotion/utils/__init__.py
+++ flumotion/utils/__init__.py
@@ -4,8 +4,6 @@
 # Flumotion - a video streaming server
 # Copyright (C) 2004 Fluendo
 #
-# flumotion/utils/__init__.py: Flumotion utility functions
-# 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
@@ -19,36 +17,3 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Street #330, Boston, MA 02111-1307, USA.
-
-"""
-This module provides utility functions for Flumotion.
-"""
-
-import sys
-from twisted.python.rebuild import rebuild
-from flumotion.utils import log
-def reload():
-    """Properly reload all flumotion-related modules currently loaded."""
-    _ignore = (
-        'flumotion.twisted.pygtk',
-        'flumotion.twisted.gst',
-        'flumotion.twisted.gobject',
-        # added because for some reason rebuilding it makes all log.Loggable
-        # subclass objects lose their log methods ...
-        'flumotion.utils.log',
-    )
-    for name in sys.modules.keys():
-        if name in _ignore:
-            continue
-        if not name.startswith('flumotion'):
-            continue
-
-        if not sys.modules.has_key(name):
-            log.warning("reload", "hm, %s disappeared from the modules" % name)
-            continue
-        module = sys.modules[name]
-        if not module:
-            log.log("reload", "hm, module '%s' is None" % name)
-            continue
-        log.log("reload", "rebuilding %s" % module)
-        rebuild(module, doLog=0)
