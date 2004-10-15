172f641910d5b2fbd6492d57ab7fabf57a1e351e
Thomas Vander Stichele <thomas@apestaart.org>
2004-10-15 14:09:13 +0000

    add missing files

---
+++ flumotion/twisted/reflect.py
@@ -0,0 +1,65 @@
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+#
+# flumotion/twisted/reflect.py: see twisted.python.reflect
+#
+# Flumotion - a streaming media server
+# Copyright (C) 2004 Fluendo (www.fluendo.com)
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# See "LICENSE.GPL" in the source distribution for more information.
+
+# This program is also licensed under the Flumotion license.
+# See "LICENSE.Flumotion" in the source distribution for more information.
+
+"""
+Functions based on twisted.python.reflect
+"""
+
+import md5
+
+from twisted.cred import checkers, credentials
+from twisted.cred.portal import IRealm, Portal
+from twisted.internet import protocol
+from twisted.python import log, reflect
+from twisted.spread import pb, flavors
+from twisted.spread.pb import PBClientFactory
+
+### stolen from twisted.python.reflect and changed
+### the version in Twisted 1.3.0 checks length of backtrace as metric for
+### ImportError; for me this fails because two lines of ihooks.py are in
+### between
+### filed as http://www.twistedmatrix.com/users/roundup.twistd/twisted/issue698
+### remove this when fixed and depending on new upstream twisted
+def namedAny(name):
+    """Get a fully named package, module, module-global object, or attribute.
+    """
+    names = name.split('.')
+    topLevelPackage = None
+    moduleNames = names[:]
+    while not topLevelPackage:
+        try:
+            trialname = '.'.join(moduleNames)
+            topLevelPackage = __import__(trialname)
+        except ImportError:
+            import sys
+            # if the ImportError happened in the module being imported,
+            # this is a failure that should be handed to our caller.
+            shortname = trialname.split('.')[-1]
+            r = str(sys.exc_info()[1])
+            if not (r.startswith('No module named') and
+                    r.endswith(shortname)):
+                raise
+            
+            #if str(sys.exc_info()[1]) != "No module named %s" % trialname:
+            #    raise
+            moduleNames.pop()
+                                                                                
+    obj = topLevelPackage
+    for n in names[1:]:
+        obj = getattr(obj, n)
+                                                                                
+    return obj
