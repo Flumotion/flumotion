a2ef1921cfb0a2193b0b35ce0b5b7c33b7c9dd0a
Johan Dahlin <johan@flumotion.com>
2004-10-15 14:39:23 +0000

    Build fixes

---
+++ flumotion/component/muxers/multipart.py
@@ -0,0 +1,35 @@
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+#
+# flumotion/component/muxers/ogg.py: ogg multiplexer
+#
+# Flumotion - a streaming media server
+# Copyright (C) 2004 Fluendo (www.fluendo.com)
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# See "LICENSE.GPL" in the source distribution for more information.
+
+# This program is also licensed under the Flumotion license.
+# See "LICENSE.Flumotion" in the source distribution for more information.
+
+from flumotion.component import feedcomponent
+
+class Multipart(feedcomponent.ParseLaunchComponent):
+    def __init__(self, name, sources, pipeline):
+        feedcomponent.ParseLaunchComponent.__init__(self, name,
+                                                    sources,
+                                                    ['default'],
+                                                    pipeline)
+
+def createComponent(config):
+    pipeline = 'multipartmux name=muxer '
+    for eater in config['source']:
+        pipeline += '{ @ eater:%s @ ! queue } ! muxer. ' % eater
+    pipeline += 'muxer.'
+    
+    component = Multipart(config['name'], config['source'], pipeline)
+    
+    return component
