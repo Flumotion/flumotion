976c80ea04567a75b53a89d433ab441a2132a186
Thomas Vander Stichele <thomas@apestaart.org>
2004-10-15 13:45:49 +0000

    more cleanups

--- flumotion/twisted/pb.py
+++ flumotion/twisted/pb.py
@@ -28,6 +28,7 @@ from twisted.python import log, reflect
 from twisted.spread import pb, flavors
 from twisted.spread.pb import PBClientFactory
 
+from flumotion.twisted import reflect as freflect
 # TODO:
 #   subclass FMClientFactory
 #   merge FMCF back into twisted
@@ -199,41 +200,6 @@ class FMClientFactory(pb.PBClientFactory):
                       client,
                       interfaces)
         return d
-### stolen from twisted.python.reflect and changed
-### the version in Twisted 1.3.0 checks length of backtrace as metric for
-### ImportError; for me this fails because two lines of ihooks.py are in
-### between
-### filed as http://www.twistedmatrix.com/users/roundup.twistd/twisted/issue698
-### remove this when fixed and depending on new upstream twisted
-def namedAny(name):
-    """Get a fully named package, module, module-global object, or attribute.
-    """
-    names = name.split('.')
-    topLevelPackage = None
-    moduleNames = names[:]
-    while not topLevelPackage:
-        try:
-            trialname = '.'.join(moduleNames)
-            topLevelPackage = __import__(trialname)
-        except ImportError:
-            import sys
-            # if the ImportError happened in the module being imported,
-            # this is a failure that should be handed to our caller.
-            shortname = trialname.split('.')[-1]
-            r = str(sys.exc_info()[1])
-            if not (r.startswith('No module named') and
-                    r.endswith(shortname)):
-                raise
-            
-            #if str(sys.exc_info()[1]) != "No module named %s" % trialname:
-            #    raise
-            moduleNames.pop()
-                                                                                
-    obj = topLevelPackage
-    for n in names[1:]:
-        obj = getattr(obj, n)
-                                                                                
-    return obj
 
 ### FIXME: this code is an adaptation of twisted/spread/pb.py
 # it allows you to login to a PB server requesting interfaces other than
@@ -262,7 +228,7 @@ class _PortalWrapper(pb.Referenceable):
     def remote_login(self, username, avatarId, *interfaces):
         # corresponds with FMClientFactory._cbSendUsername
         """Start of username/password login."""
-        interfaces = [namedAny(interface) for interface in interfaces]
+        interfaces = [freflect.namedAny(interface) for interface in interfaces]
         c = pb.challenge()
         return c, _PortalAuthChallenger(self, username, avatarId, c, *interfaces)
 
