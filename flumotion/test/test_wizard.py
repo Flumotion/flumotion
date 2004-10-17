62c351ffadf74e36a07a95ad431061f4d9520197
Johan Dahlin <johan@flumotion.com>
2004-10-17 10:40:13 +0000

    Add very simple unittest for wizard and its steps

---
+++ flumotion/test/test_wizard.py
@@ -0,0 +1,40 @@
+# -*- Mode: Python; test-case-name: flumotion.test.test_wizard -*-
+# vi:si:et:sw=4:sts=4:ts=4
+#
+# flumotion/test/test_wizard.py:
+# regression test for flumotion.wizard
+#
+# Flumotion - a streaming media server
+# Copyright (C) 2004 Fluendo (www.fluendo.com)
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# See "LICENSE.GPL" in the source distribution for more information.
+
+# This program is also licensed under the Flumotion license.
+# See "LICENSE.Flumotion" in the source distribution for more information.
+
+import common
+import gtk
+
+from twisted.trial import unittest
+
+from flumotion.wizard import wizard
+
+class WizardStepTest(unittest.TestCase):
+    def testLoadSteps(self):
+        import flumotion.wizard.steps
+        
+        for step in wizard.wiz.steps:
+            self.assert_(isinstance(step, wizard.WizardStep))
+            self.assert_(hasattr(step, 'icon'))
+            windows = [widget for widget in step.widgets
+                                  if isinstance(widget, gtk.Window)]
+            self.assert_(len(windows) == 1)
+            window = windows[0]
+            self.failIf(window.get_property('visible'))
+            self.assert_(hasattr(step, 'icon'))
+            self.assert_(hasattr(step, 'glade_file'))
+            self.assert_(hasattr(step, 'step_name'))
--- flumotion/wizard/wizard.py
+++ flumotion/wizard/wizard.py
@@ -1,4 +1,4 @@
-# -*- Mode: Python -*-
+# -*- Mode: Python; test-case-name: flumotion.test.test_wizard -*-
 # vi:si:et:sw=4:sts=4:ts=4
 #
 # flumotion/wizard/wizard.py: the configuration wizard
@@ -570,7 +570,6 @@ def run(interactive=True):
     
     import flumotion.wizard.steps
     wiz.run(interactive)
-    
 
 if __name__ == '__main__':
     run()
