7734696cc1b3a4e9d04615921947c74dc51af9ce
Johan Dahlin <johan@flumotion.com>
2004-10-19 18:44:40 +0000

    script to fix headers

---
+++ tools/fixheader.py
@@ -0,0 +1,28 @@
+import sys
+
+# Copyright (C) 2004 Fluendo (www.fluendo.com)
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# See "LICENSE.GPL" in the source distribution for more information.
+
+# This program is also licensed under the Flumotion license.
+# See "LICENSE.Flumotion" in the source distribution for more information.
+
+orig = """# See "LICENSE.Flumotion" in the source distribution for more information.
+"""
+
+new = """# See "LICENSE.Flumotion" in the source distribution for more information.
+
+# Headers in this file shall remain intact.
+"""
+
+for name in sys.argv[1:]:
+    fd = open(name)
+    data = fd.read()
+    fd.close()
+    
+    data = data.replace(orig, new)
+    open(name, 'w').write(data)
