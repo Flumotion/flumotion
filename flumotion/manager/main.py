b90be7ef3ca9df22f36c2dbb2f8b6a0c17e415f9
Thomas Vander Stichele <thomas@apestaart.org>
2004-10-19 12:21:32 +0000

    spec updates, ifxed -H bu

--- flumotion/manager/main.py
+++ flumotion/manager/main.py
@@ -42,7 +42,7 @@ class ServerContextFactory:
 def _startSSL(vishnu, host, port, pemFile):
     # if no path in pemFile, then look for it in the config directory
     if not os.path.split(pemFile)[0]:
-        pemFile = os.path.join(configure.configdir, 'manager', pemFile)
+        pemFile = os.path.join(configure.configdir, 'managers', 'default', pemFile)
     if not os.path.exists(pemFile):
         log.error('manager', ".pem file %s does not exist" % pemFile)
     log.debug('manager', 'Using PEM certificate file %s' % pemFile)
