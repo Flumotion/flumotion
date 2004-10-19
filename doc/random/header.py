d6708a42a6dd873ccad0201953fefab0e796c10d
Thomas Vander Stichele <thomas@apestaart.org>
2004-10-19 14:50:30 +0000

    semifinal header

--- doc/random/header.py
+++ doc/random/header.py
@@ -4,13 +4,16 @@
 # doc/random/header.py: template header
 #
 # Flumotion - a streaming media server
-# Copyright (C) 2004 Fluendo (www.fluendo.com)
+# Copyright (C) 2004 Fluendo, S.L. (www.fluendo.com). All rights reserved.
 
 # This file may be distributed and/or modified under the terms of
 # the GNU General Public License version 2 as published by
 # the Free Software Foundation.
+# This file is distributed without any warranty; without even the implied
+# warranty of merchantability or fitness for a particular purpose.
 # See "LICENSE.GPL" in the source distribution for more information.
 
-# Licensees holding valid Flumotion Advanced licenses may use this file
-# in accordance with the Flumotion Advanced Commercial License Agreement.
+# Licensees having purchased or holding a valid Flumotion Advanced
+# Streaming Server license may use this file in accordance with the
+# Flumotion Advanced Streaming Server Commercial License Agreement.
 # See "LICENSE.Flumotion" in the source distribution for more information.
