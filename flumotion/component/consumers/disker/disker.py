0f8dbbe6fe2b0a199fcbaf3be9dbd2bcd9721057
Thomas Vander Stichele <thomas@apestaart.org>
2005-07-24 12:13:45 +0000

    scrub header

--- flumotion/component/consumers/disker/disker.py
+++ flumotion/component/consumers/disker/disker.py
@@ -1,8 +1,6 @@
 # -*- Mode: Python -*-
 # vi:si:et:sw=4:sts=4:ts=4
 #
-# flumotion/component/consumers/disker/disker.py: archive to disk
-#
 # Flumotion - a streaming media server
 # Copyright (C) 2004,2005 Fluendo, S.L. (www.fluendo.com). All rights reserved.
 
