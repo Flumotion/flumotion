26f437d7d33c0531e53ea42a5aea247c445ec5b3
Michael Smith <msmith@fluendo.com>
2005-09-15 12:16:39 +0000

    Check for 2.8, not 1.8

--- flumotion/common/compat.py
+++ flumotion/common/compat.py
@@ -29,7 +29,7 @@ import gobject
 # gobject.type_register() if we don't need it
 def type_register(type):
   (major, minor, patch) = gtk.pygtk_version
-  if(major <= 1 and minor < 8):
+  if(major <= 2 and minor < 8):
     gobject.type_register(type)
   elif(not (hasattr(type, '__gtype_name__' or hasattr(type, '__gproperties__')
          or hasattr(type, '__gsignals__'))):
