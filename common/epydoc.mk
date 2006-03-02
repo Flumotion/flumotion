60470e9072bfe0613f3f764139714e0b63017869
Thomas Vander Stichele <thomas@apestaart.org>
2006-03-02 13:07:55 +0000

    too much stuff on the cmd line, shut it up

--- common/epydoc.mk
+++ common/epydoc.mk
@@ -10,7 +10,7 @@ pypath = $(top_builddir):$(top_srcdir):$(PYTHONPATH)
 
 html/index.html: $(patsubst %, $(top_srcdir)/%, $(MODULE_FILES)) $(top_srcdir)/common/gendoc.py
 	@echo Generating HTML documentation...
-	PYTHONPATH=$(pypath) $(PYTHON) $(top_srcdir)/common/gendoc.py $(EPYDOC_ARGS) $(MODULES)
+	@PYTHONPATH=$(pypath) $(PYTHON) $(top_srcdir)/common/gendoc.py $(EPYDOC_ARGS) $(MODULES)
 
 all-local-epydoc: html/index.html
 
