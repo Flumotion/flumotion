33e869e0637a4a1ccf269b70d964efc75520198c
Thomas Vander Stichele <thomas@apestaart.org>
2006-04-14 09:38:07 +0000

    fixes to pychecker.mk
            * common/pychecker.mk:
              - use $(thisdir) correctly when we check for _build
              - rename FLU_PATH to OUR_PATH, because these macros are shared
                with other modules
              - if GST_08_SUPPORTED and GST_010_SUPPORTED are not set from
                configure, still work without errors
              - x10 -> x010 for consistency

--- common/pychecker.mk
+++ common/pychecker.mk
@@ -12,23 +12,24 @@ pychecker =					\
 	$(pychecker_help)
 
 # during distcheck, we get executed from $(NAME)-$(VERSION)/_build, while
-# our python sources are one level up.  Figure this out and set a FLU_PATH
+# our python sources are one level up.  Figure this out and set a OUR_PATH
 # this uses Makefile syntax, so we need to protect it from automake
 thisdir = $(shell basename `pwd`)
-FLU_PATH = $(if $(subst _build,,$(cur)),$(shell pwd),$(shell pwd)/..)
+OUR_PATH = $(if $(subst _build,,$(thisdir)),$(shell pwd),$(shell pwd)/..)
 
-# TODO: This looks a little confusing because out 0.10 files are names blah010.py
+# TODO: This looks a little confusing because our 0.10 files are named
+# blah010.py
 pychecker_all_files = $(filter-out $(PYCHECKER_BLACKLIST),$(wildcard $(PYCHECKER_WHITELIST)))
 pychecker_08_files = $(filter %08.py,$(pychecker_all_files))
 pychecker_010_files = $(filter %010.py,$(pychecker_all_files))
 pychecker_indep_files = $(filter-out $(pychecker_08_files) $(pychecker_010_files),$(pychecker_all_files))
 
-pychecker_indep = PYTHONPATH=$(FLU_PATH) $(pychecker)
-pychecker_08 = PYTHONPATH=$(PYGST_08_DIR):$(FLU_PATH) FLU_GST_VERSION=0.8 $(pychecker)
-pychecker_010 = PYTHONPATH=$(PYGST_010_DIR):$(FLU_PATH) FLU_GST_VERSION=0.10 $(pychecker)
+pychecker_indep = PYTHONPATH=$(OUR_PATH) $(pychecker)
+pychecker_08 = PYTHONPATH=$(PYGST_08_DIR):$(OUR_PATH) FLU_GST_VERSION=0.8 $(pychecker)
+pychecker_010 = PYTHONPATH=$(PYGST_010_DIR):$(OUR_PATH) FLU_GST_VERSION=0.10 $(pychecker)
 
-pychecker_if_08 = if test $(GST_08_SUPPORTED) = yes; then 
-pychecker_if_010 = if test $(GST_010_SUPPORTED) = yes; then 
+pychecker_if_08 = if test "x$(GST_08_SUPPORTED)" = "xyes"; then 
+pychecker_if_010 = if test "x$(GST_010_SUPPORTED)" = "xyes"; then 
 pychecker_fi = else echo "passing, gstreamer version not supported"; fi
 
 # we redirect stderr so we don't get messages like
@@ -45,7 +46,7 @@ pycheckersplit:
 		fi							\
 	done
 
-pychecker: pychecker08 pychecker10 pycheckerindep
+pychecker: pychecker08 pychecker010 pycheckerindep
 	@true
 
 pycheckerindep: 
@@ -57,12 +58,12 @@ pychecker08:
 	@$(pychecker_if_08) $(pychecker_08) $(pychecker_08_files) 2>/dev/null \
 	  || make pycheckerverbose08; $(pychecker_fi)
 
-pychecker10:
+pychecker010:
 	@echo running pychecker, gstreamer 0.10-specific code ...
 	@$(pychecker_if_010) $(pychecker_010) $(pychecker_010_files) 2>/dev/null \
-	  || make pycheckerverbose10; $(pychecker_fi)
+	  || make pycheckerverbose010; $(pychecker_fi)
 
-pycheckerverbose: pycheckerverbose08 pycheckerverbose10 pycheckerverboseindep
+pycheckerverbose: pycheckerverbose08 pycheckerverbose010 pycheckerverboseindep
 
 pycheckerverboseindep:
 	@echo "running pychecker, gstreamer-agnostic files (verbose) ..."
@@ -72,6 +73,6 @@ pycheckerverbose08:
 	@echo "running pychecker, gstreamer 0.8-specific code (verbose) ..."
 	$(pychecker_if_08) $(pychecker_08) $(pychecker_08_files); $(pychecker_fi)
 
-pycheckerverbose10:
+pycheckerverbose010:
 	@echo "running pychecker, gstreamer 0.10-specific code (verbose) ..."
 	$(pychecker_if_010) $(pychecker_010) $(pychecker_010_files); $(pychecker_fi)
