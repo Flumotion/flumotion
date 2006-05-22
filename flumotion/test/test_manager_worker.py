ba00c565a9dfd555648c68fc7ae73db785d84052
Zaheer Abbas Merali <zaheerabbas at merali dot org>
2006-05-22 18:10:35 +0000

    2006-05-22  Zaheer Abbas Merali  <zaheerabbas at merali dot org>
            * flumotion/test/test_manager_worker.py:
            Fix failing TestHeaven.testAttached test.

--- flumotion/test/test_manager_worker.py
+++ flumotion/test/test_manager_worker.py
@@ -22,6 +22,61 @@
 from twisted.trial import unittest
 
 from flumotion.manager import worker
+from flumotion.common import log
+
+class FakeTransport:
+    def getPeer(self):
+        from twisted.internet.address import IPv4Address
+        return IPv4Address('TCP', 'nullhost', 1)
+    def getHost(self):
+        from twisted.internet.address import IPv4Address
+        return IPv4Address('TCP', 'nullhost', 1)
+
+class FakeBroker:
+    def __init__(self):
+        self.transport = FakeTransport()
+
+class FakeMind(log.Loggable):
+    def __init__(self, testcase):
+        self.broker = FakeBroker()
+        self.testcase = testcase
+
+    def callRemote(self, name, *args, **kwargs):
+        self.debug('callRemote(%s, %r, %r)' % (name, args, kwargs))
+        #print "callRemote(%s, %r, %r)" % (name, args, kwargs)
+        method = "remote_" + name
+        if hasattr(self, method):
+            m = getattr(self, method)
+            try:
+                result = m(*args, **kwargs)
+                self.debug('callRemote(%s) succeeded with %r' % (
+                    name, result))
+                return defer.succeed(result)
+            except Exception, e:
+                self.warning('callRemote(%s) failed with %s: %s' % (
+                    name, str(e.__class__), ", ".join(e.args)))
+                return defer.fail(e)
+        else:
+            raise AttributeError('no method %s on self %r' % (name, self))
+
+
+class FakeWorkerMind(FakeMind):
+
+    logCategory = 'fakeworkermind'
+    
+    def __init__(self, testcase, avatarId):
+        FakeMind.__init__(self, testcase)
+        self.avatarId = avatarId
+
+    def remote_getPorts(self):
+        return range(7600,7610)
+
+    def remote_create(self, avatarId, type, moduleName, methodName, config):
+        self.debug('remote_create(%s): logging in component' % avatarId)
+        avatar = self.testcase._loginComponent(self.avatarId,
+            avatarId, moduleName, methodName, type, config)
+        # need to return the avatarId for comparison
+        return avatarId
 
 class TestHeaven(unittest.TestCase):
     def testConstructor(self):
@@ -43,6 +98,9 @@ class TestHeaven(unittest.TestCase):
 
     def testAttached(self):
         h = worker.WorkerHeaven(None)
+        # need to create fake mind so workerAttached works
+        mind = FakeWorkerMind(self, 'testworker')
         avatar = h.createAvatar('foo', None)
+        avatar.attached(mind)
 
         h.workerAttached(avatar)
