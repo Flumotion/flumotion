80c9f440510dedd0da660258ebd4604721c05178
Thomas Vander Stichele <thomas@apestaart.org>
2006-07-11 19:16:17 +0000

    * flumotion/test/testclasses.py: 	  add a TestPB class that implements a PB connection with .send 	  and .receive for easier testing 	* flumotion/test/test_testclasses.py: 	  add a test for our testclasses 	  show possible implementation of Keycard using Cacheable/RemoteCache 	  and still have a working observer, as well as control over the 	  exact cached state 	* flumotion/test/Makefile.am: 	  add test_testclasses

---
+++ flumotion/test/test_testclasses.py
@@ -0,0 +1,222 @@
+# -*- Mode: Python; test-case-name: flumotion.test.test_testclasses -*-
+# vi:si:et:sw=4:sts=4:ts=4
+#
+# Flumotion - a streaming media server
+# Copyright (C) 2004,2005,2006 Fluendo, S.L. (www.fluendo.com).
+# All rights reserved.
+
+# This file may be distributed and/or modified under the terms of
+# the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+# This file is distributed without any warranty; without even the implied
+# warranty of merchantability or fitness for a particular purpose.
+# See "LICENSE.GPL" in the source distribution for more information.
+
+# Licensees having purchased or holding a valid Flumotion Advanced
+# Streaming Server license may use this file in accordance with the
+# Flumotion Advanced Streaming Server Commercial License Agreement.
+# See "LICENSE.Flumotion" in the source distribution for more information.
+
+# Headers in this file shall remain intact.
+
+import common
+from twisted.trial import unittest
+
+import testclasses
+
+from twisted.internet import defer
+from twisted.spread import pb, jelly
+
+from flumotion.common import log
+
+# an object that subclasses from both Cacheable and RemoteCache
+# can be serialized over more than one PB connection
+# state changes on the "master" still get serialized to the slaves
+class FakeKeycard(pb.Cacheable, pb.RemoteCache):
+    name = None
+    observers = None
+
+    def getStateToCacheAndObserveFor(self, perspective, observer):
+        # we can't implement __init__ due to pb.RemoteCache, so ...
+        if not self.observers: self.observers = []
+        self.observers.append(observer)
+        return {'name': self.name}
+
+    # since we inherit from both classes, each with their own jellyFor,
+    # we need to make sure they get called correctly
+
+    def jellyFor(self, jellier):
+        if not self.broker:
+            # since we don't have a broker yet, this is the first time
+            # we go through one, so we are the "master" and we should
+            # be jellied as a cacheable
+            return pb.Cacheable.jellyFor(self, jellier)
+
+        if jellier.invoker and jellier.invoker is not self.broker:
+            # we are being sent through a different broker than the one
+            # that actually created us, so again, treat us as a cacheable
+            return pb.Cacheable.jellyFor(self, jellier)
+            
+        # we've already been seen by a broker, so we should be jellied as
+        # a remotecache
+        return pb.RemoteCache.jellyFor(self, jellier)
+
+    def setName(self, name):
+        l = []
+        self.name = name
+        if self.observers: # since it can be None, see class def
+            for o in self.observers:
+                d = o.callRemote('setName', name)
+                l.append(d)
+        return defer.DeferredList(l)
+
+    def observe_setName(self, name):
+        log.debug("fakekeycard", "%r.observe_setName, %r" % (self, name))
+        # invoke the setter so our RemoteCache's in turn get notified
+        return self.setName(name)
+        
+    def stoppedObserving(self, perspective, observer):
+            self.observers.remove(observer)
+
+pb.setUnjellyableForClass(FakeKeycard, FakeKeycard)
+
+class FakeCacheable(pb.Cacheable): pass
+class FakeRemoteCache(pb.RemoteCache): pass
+pb.setUnjellyableForClass(FakeCacheable, FakeRemoteCache)
+
+class TestOnePB(unittest.TestCase):
+    def setUp(self):
+        self.pb = testclasses.TestPB()
+        return self.pb.start()
+
+    def tearDown(self):
+        return self.pb.stop()
+        
+    def testFakeKeycard(self):
+        keycard = FakeKeycard()
+        keycard.name = "tarzan"
+        keycard.password = "jane"
+
+        def _send():
+            # sending it should result in a keycard that does not have password
+            d = self.pb.send(keycard)
+            return d
+
+        def _sendCb(sent):
+            self.failUnless(sent)
+            self.failIfEquals(keycard, sent)
+            self.failIf(hasattr(sent, "password"))
+
+            # receiving it again should give us back the original keycard
+            d = self.pb.receive(sent)
+            return d
+
+        def _receiveCb(received):
+            self.failUnless(received)
+            self.assertEquals(keycard, received)
+            self.assertEquals(received.password, "jane")
+
+        d = _send()
+        d.addCallback(_sendCb)
+        d.addCallback(_receiveCb)
+        return d
+
+    def testCacheable(self):
+        c = FakeCacheable()
+
+        def _receive(sent):
+            # we can send it back though
+            d = self.pb.receive(sent)
+            return d
+
+        def _receiveCb(received):
+            self.assertEquals(c, received)
+
+        d = self.pb.send(c)
+        d.addCallback(_receive)
+        d.addCallback(_receiveCb)
+        return d
+
+class TestTwoPB(unittest.TestCase):
+    # test if our classes work over two chained PB connections
+    def setUp(self):
+        self.pb1 = testclasses.TestPB()
+        self.pb2 = testclasses.TestPB()
+        d = self.pb1.start()
+        d.addCallback(lambda r: self.pb2.start())
+        return d
+
+    def tearDown(self):
+        d = self.pb1.stop()
+        d.addCallback(lambda r: self.pb2.stop())
+        return d
+        
+    def testFakeKeycard(self):
+        keycard = FakeKeycard()
+        keycard.name = "tarzan"
+        keycard.password = "jane"
+
+        def _send1():
+            # sending it should result in a keycard that does not have password
+            d = self.pb1.send(keycard)
+            return d
+
+        def _send1Cb(sent1):
+            self.failUnless(sent1)
+            self.failIfEquals(keycard, sent1)
+            self.failIf(hasattr(sent1, "password"))
+            return sent1
+
+        def _send2(sent1):
+            log.debug('TestTwoPB', 'sending sent1 %r' % sent1)
+            self.sent1 = sent1
+            d = self.pb2.send(sent1)
+            return d
+
+        def _send2Cb(sent2):
+            self.failUnless(sent2)
+            self.failIfEquals(keycard, sent2)
+            self.failIf(hasattr(sent2, "password"))
+            return sent2
+
+        def _receive2(sent2):
+            self.sent2 = sent2
+            d = self.pb2.receive(sent2)
+            return d
+
+        def _receive2Cb(received2):
+            # receive in pb 2; ie "in the middle"
+            self.failUnless(received2)
+            self.assertEquals(self.sent1, received2)
+            return received2
+
+        def _receive1(received2):
+            d = self.pb1.receive(received2)
+            return d
+            
+        def _receive1Cb(received1):
+            # receive in pb 1
+            self.failUnless(received1)
+            self.assertEquals(keycard, received1)
+            self.assertEquals(received1.password, "jane")
+            return received1
+
+        def _setName(result):
+            # show that setting the name on the original object gets
+            # proxied
+            return keycard.setName('mowgli')
+
+        def _setNameCb(result):
+            self.assertEquals(self.sent2.name, 'mowgli')
+
+        d = _send1()
+        d.addCallback(_send1Cb)
+        d.addCallback(_send2)
+        d.addCallback(_send2Cb)
+        d.addCallback(_receive2)
+        d.addCallback(_receive2Cb)
+        d.addCallback(_setName)
+        d.addCallback(_setNameCb)
+        return d
+
+
--- flumotion/test/testclasses.py
+++ flumotion/test/testclasses.py
@@ -1,4 +1,4 @@
-# -*- Mode: Python -*-
+# -*- Mode: Python; test-case-name: flumotion.test.test_testclasses -*-
 # vi:si:et:sw=4:sts=4:ts=4
 #
 # Flumotion - a streaming media server
@@ -19,15 +19,29 @@
 
 # Headers in this file shall remain intact.
 
+import common
+
 from twisted.internet import reactor, defer
 from twisted.spread import pb
+
+from flumotion.common import log
 
 # test objects to be used in unittests to simulate the processes
 # subclass them to add your own methods
 
 class TestClient(pb.Referenceable):
+
+    type = "client" # override in subclass
+    remoteRoot = None # RemoteReference to the server-side root
+    
     def run(self, port):
-        self.perspective = None # perspective on the manager's PB server
+        """
+        Start the client by connecting to the server on the given port.
+
+        @type port: int
+
+        @rtype: L{defer.Deferred}
+        """
         self._f = pb.PBClientFactory()
         self._p = reactor.connectTCP("127.0.0.1", port, self._f)
         d = self._f.getRootObject()
@@ -35,18 +49,27 @@ class TestClient(pb.Referenceable):
         return d
 
     def stop(self):
+        """
+        Stop the client.
+
+        @rtype: L{defer.Deferred}
+        """
         self._p.disconnect()
         return self._dDisconnect
 
-    def _gotRootObject(self, perspective):
-        self.perspective = perspective
+    def _gotRootObject(self, remoteReference):
+        self.remoteRoot = remoteReference
 
         # make sure we will get a deferred fired on disconnect
         # so that the broker gets cleaned up from the reactor as well
         self._dDisconnect = defer.Deferred()
-        self.perspective.notifyOnDisconnect(
+        self.remoteRoot.notifyOnDisconnect(
             lambda r: self._dDisconnect.callback(None))
-        return perspective.callRemote('identify', self.type, self)
+        return self.remoteRoot.callRemote('identify', self.type, self)
+
+    def remote_receive(self, object):
+        # called by the server to send us an object
+        self.object = object
 
 class TestAdmin(TestClient):
     type = 'admin'
@@ -54,21 +77,81 @@ class TestAdmin(TestClient):
 class TestWorker(TestClient):
     type = 'worker'
 
-class TestManagerRoot(pb.Root):
+class TestManagerRoot(pb.Root, log.Loggable):
+    logCategory = "testmanagerroot"
     def remote_identify(self, who, reference):
+        """
+        Called by a TestClient to announce the type of client, and give
+        a reference.
+        """
+        self.debug('remote_identify: who %r, ref %r' % (who, reference))
         key = who + 'Reference'
         setattr(self, key, reference)
+
+    def remote_receive(self, object):
+        # called by the client to send us an object
+        self.object = object
 
 class TestManager:
     def run(self, rootClass):
         """
         Run the test manager.  Return port it is listening on.
+
+        @type  rootClass: subclass of L{TestManagerRoot}
+
+        @rtype: int
         """
-        factory = pb.PBServerFactory(rootClass())
+        self.root = rootClass()
+        factory = pb.PBServerFactory(self.root)
         factory.unsafeTracebacks = 1
         self._p = reactor.listenTCP(0, factory, interface="127.0.0.1")
         port = self._p.getHost().port
         return port
 
     def stop(self):
+        """
+        Stop the server.
+        """
         return self._p.stopListening()
+
+class TestPB(log.Loggable):
+    """
+    I combine a manager and a client to test passing back and forth objects.
+    """
+    logCategory = "testpb"
+
+    def __init__(self):
+        self.manager = TestManager()
+        self.client = TestClient()
+
+    def start(self):
+        port = self.manager.run(TestManagerRoot)
+        return self.client.run(port)
+
+    def stop(self):
+        d = self.manager.stop()
+        d.addCallback(lambda r: self.client.stop())
+        return d
+
+    def send(self, object):
+        """
+        Send the object from client to server.
+        Return the server's idea of the object.
+        """
+        self.debug('sending object %r from broker %r' % (
+            object, self.client.remoteRoot.broker))
+        d = self.client.remoteRoot.callRemote('receive', object)
+        d.addCallback(lambda r: self.manager.root.object)
+        return d
+        
+    def receive(self, object):
+        """
+        Receive the object from server to client.
+        Return the client's idea of the object.
+        """
+        self.debug('receiving object %r' % object)
+        d = self.manager.root.clientReference.callRemote('receive', object)
+        d.addCallback(lambda r: self.client.object)
+        return d
+        
+     
