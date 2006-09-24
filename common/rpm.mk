3602a8e66b8e09ad04d938f29f4d8d427cc55ac9
Thomas Vander Stichele <thomas@apestaart.org>
2006-09-24 21:05:59 +0000

    * Makefile.am: 	* common/Makefile.am: 	* common/rpm.mk: 	  add an "rpm" target that builds rpms

---
+++ common/rpm.mk
@@ -0,0 +1,7 @@
+rpm: dist
+	for i in RPMS SRPMS SOURCES SPECS BUILD; do mkdir -p rpmbuild/$$i; done
+	cp flumotion-*.tar.bz2 rpmbuild/SOURCES
+	rpmbuild --define "_topdir `pwd`/rpmbuild" -ba flumotion.spec
+	mv rpmbuild/RPMS/*/flumotion*.rpm .
+	mv rpmbuild/SRPMS/flumotion*.rpm .
+	rm -rf rpmbuild
