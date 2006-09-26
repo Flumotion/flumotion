cf397bd5c678f7a57df1bb94c67ed48ad8a6f6a1
Thomas Vander Stichele <thomas@apestaart.org>
2006-09-26 13:43:38 +0000

    remove rpms first

--- common/rpm.mk
+++ common/rpm.mk
@@ -1,4 +1,5 @@
 rpm: dist
+	rm *.rpm
 	for i in RPMS SRPMS SOURCES SPECS BUILD; do mkdir -p rpmbuild/$$i; done
 	cp flumotion-*.tar.bz2 rpmbuild/SOURCES
 	rpmbuild --define "_topdir `pwd`/rpmbuild" -ba flumotion.spec
