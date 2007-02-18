e4ae3ba84c2d31ca3292165e7c73ccf7f1959c84
Thomas Vander Stichele <thomas@apestaart.org>
2007-02-18 22:49:25 +0000

    * flumotion/component/bouncers/icalbouncer.py: 	  a stray tab was breaking trial --coverage; expand it

--- flumotion/component/bouncers/icalbouncer.py
+++ flumotion/component/bouncers/icalbouncer.py
@@ -34,7 +34,7 @@ __all__ = ['IcalBouncer']
 
 try:
     # icalendar and dateutil modules needed for ical parsing
-	from icalendar import Calendar
+    from icalendar import Calendar
     from dateutil import rrule
     HAS_ICAL = True
 except:
