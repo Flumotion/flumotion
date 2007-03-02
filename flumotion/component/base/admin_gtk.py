9c51a8eb2057a205cc4b3b6fc0f6b47e9cdb44d0
Sebastien Merle <sebastien@fluendo.com>
2007-03-02 15:03:48 +0000

    2007-03-02  Sebastien Merle  <sebastien@fluendo.com>
    	* flumotion/component/base/admin_gtk.py: 
    	Added more comments and changed a class variable name
    	to make it less ambiguous.

--- flumotion/component/base/admin_gtk.py
+++ flumotion/component/base/admin_gtk.py
@@ -192,7 +192,8 @@ class BaseAdminGtkNode(log.Loggable):
     implements(flavors.IStateListener)
 
     logCategory = "admingtk"
-    glade_file = None
+    glade_file = None ## Relative path of the glade file.
+                      ##   e.g. "flumotion/ui.glade"
     gettext_domain = 'flumotion'
 
     def __init__(self, state, admin, title=None):
@@ -212,7 +213,10 @@ class BaseAdminGtkNode(log.Loggable):
         self.wtree = None
         self.widget = None
         self.uiState = None
-        self.gladefile = None
+        ## Absolute path to the glade file.
+        ##   e.g. "/home/flu/.flumotion/cache/test/80...df7/flumotion/ui.glade
+        self._gladefilepath = None 
+        
         
     def status_push(self, str):
         if self.statusbar:
@@ -262,7 +266,7 @@ class BaseAdminGtkNode(log.Loggable):
                     gladeFile, path))
             self.debug("Switching glade text domain to %s" % domain)
             self.debug("loading widget tree from %s" % path)
-            self.gladefile = path
+            self._gladefilepath = path
             old = gtk.glade.textdomain()
             gtk.glade.textdomain(domain)
             self.wtree = gtk.glade.XML(path)
@@ -286,12 +290,12 @@ class BaseAdminGtkNode(log.Loggable):
 
     def createWidget(self, name):
         """
-        Create a new widget instance from the glide file.
+        Create a new widget instance from the glade file.
         Can be used to make multiple instances of the same widget.
         """
-        if not self.gladefile:
+        if not self._gladefilepath:
             raise IndexError
-        wtree = gtk.glade.XML(self.gladefile, name)
+        wtree = gtk.glade.XML(self._gladefilepath, name)
         widget = wtree.get_widget(name)
         if not widget:
             self.warning('Could not create widget %s' % name)
