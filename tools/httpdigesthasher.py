bff71375b9e35c5937e2c2627d4c3a1199d95693
Michael Smith <msmith@fluendo.com>
2007-04-10 14:24:01 +0000

    * tools/httpdigesthasher.py:           Add small tool to generate HTTP digest auth entries for files.

---
+++ tools/httpdigesthasher.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import md5
+import sys
+
+def _calculateHA1MD5(username, realm, password):
+    """
+    Calculate H(A1) as from specification (RFC2617) section 3.2.2, for the
+    MD5 algorithm.
+    """
+    m = md5.md5()
+    m.update(username)
+    m.update(':')
+    m.update(realm)
+    m.update(':')
+    m.update(password)
+    HA1 = m.digest()
+    return HA1.encode('hex')
+
+if len(sys.argv) != 3:
+    print "Usage: httpdigesthasher.py username password"
+else:
+    username = sys.argv[1]
+    # Windows Media Encoder responds with realm="", and calculates H(A1) using
+    # that value. So, we have to do the same (suck!)
+    realm = ""
+    password = sys.argv[2]
+
+    hash = _calculateHA1MD5(username, realm, password)
+    print "%s:%s" % (username, hash)
+
