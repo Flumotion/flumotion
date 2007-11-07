14ac4df87033450c8e5890e0b7c8bace3f77a216
Johan Dahlin <johan@flumotion.com>
2007-11-07 14:35:45 +0000

    2007-11-07  Johan Dahlin  <johan@gnome.org>
    	* flumotion/test/test_options.py (TestOptions): Make sure to turn
    	all logging of after running the OptionParser tests.
    	Also remove a whole copy of the file.

--- flumotion/test/test_options.py
+++ flumotion/test/test_options.py
@@ -21,72 +21,14 @@
 
 from twisted.trial import unittest
 from flumotion.common.options import OptionGroup, OptionParser
+from flumotion.common.log import reset
 
 import common
 
 class TestOptions(unittest.TestCase):
-    def testParser(self):
-        parser = OptionParser()
+    def tearDown(self):
+        reset()
 
-        options, rest = parser.parse_args(['--verbose'])
-        self.failUnless(options.verbose)
-        self.failIf(rest)
-
-        options, rest = parser.parse_args(['--debug', '*:5'])
-        self.assertEqual(options.debug, "*:5")
-        self.failIf(rest)
-
-        options, rest = parser.parse_args(['rest'])
-        self.assertEqual(rest, ["rest"])
-
-    def testGroup(self):
-        parser = OptionParser()
-
-        group = OptionGroup(parser, "group")
-        group.add_option('-t', '--test', action="store_true", dest="test")
-
-        parser.add_option_group(group)
-
-
-        options, rest = parser.parse_args([])
-        self.failIf(options.test)
-        self.failIf(rest)
-
-        options, rest = parser.parse_args(['--test'])
-        self.failUnless(options.test)
-        self.failIf(rest)
-
-        options, rest = parser.parse_args(['--test', '--verbose', 'rest'])
-        self.failUnless(options.test)
-        self.failUnless(options.verbose)
-        self.assertEqual(rest, ["rest"])
-# -*- Mode: Python; test-case-name: flumotion.test.test_options -*-
-# vi:si:et:sw=4:sts=4:ts=4
-#
-# Flumotion - a streaming media server
-# Copyright (C) 2007 Fluendo, S.L. (www.fluendo.com).
-# All rights reserved.
-
-# This file may be distributed and/or modified under the terms of
-# the GNU General Public License version 2 as published by
-# the Free Software Foundation.
-# This file is distributed without any warranty; without even the implied
-# warranty of merchantability or fitness for a particular purpose.
-# See "LICENSE.GPL" in the source distribution for more information.
-
-# Licensees having purchased or holding a valid Flumotion Advanced
-# Streaming Server license may use this file in accordance with the
-# Flumotion Advanced Streaming Server Commercial License Agreement.
-# See "LICENSE.Flumotion" in the source distribution for more information.
-
-# Headers in this file shall remain intact.
-
-from twisted.trial import unittest
-from flumotion.common.options import OptionGroup, OptionParser
-
-import common
-
-class TestOptions(unittest.TestCase):
     def testParser(self):
         parser = OptionParser()
 
