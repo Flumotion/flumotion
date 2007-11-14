e9604133c67c968e3fcc9656f4711fd26edd181b
Johan Dahlin <johan@flumotion.com>
2007-11-14 11:04:49 +0000

    2007-11-14  Johan Dahlin  <johan@gnome.org>
    	* flumotion/test/Makefile.am (EXTRA_DIST): Add test_public_ui_api.py
    	* flumotion/test/test_public_ui_api.py (TestPublicUI): 
    	New test to check our public API UI API

---
+++ flumotion/test/test_public_ui_api.py
@@ -0,0 +1,37 @@
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+#
+# Flumotion - a streaming media server
+# Copyright (C) 2004,2005,2006,2007 Fluendo, S.L. (www.fluendo.com).
+# All rights reserved.
+
+# This file may be distributed and/or modified under the terms of
+# the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+# This file is distributed without any warranty; without even the implied
+# warranty of merchantability or fitness for a particular purpose.
+# See "LICENSE.GPL" in the source distribution for more information.
+
+# Licensees having purchased or holding a valid Flumotion Advanced
+# Streaming Server license may use this file in accordance with the
+# Flumotion Advanced Streaming Server Commercial License Agreement.
+# See "LICENSE.Flumotion" in the source distribution for more information.
+
+# Headers in this file shall remain intact.
+
+from flumotion.common import testsuite
+
+class TestPublicUI(testsuite.TestCase):
+    def testPublicUIAPI(self):
+        from flumotion.admin.admin import AdminModel
+        from flumotion.admin.gtk import connections
+        from flumotion.admin.gtk import dialogs
+        from flumotion.admin.gtk import message
+        from flumotion.admin.gtk.dialogs import ProgressDialog
+        from flumotion.ui.glade import GladeWidget, GladeWindow
+        from flumotion.wizard import steps
+        from flumotion.wizard import wizard, step
+        import flumotion.wizard.steps
+
+        from flumotion.ui import icons
+        icons.register_icons()
