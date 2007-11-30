94a806fbbb87f22451cb77443d7ee2b74bd8fc7f
Johan Dahlin <johan@flumotion.com>
2007-11-30 13:07:58 +0000

    2007-11-30  Johan Dahlin  <johan@gnome.org>
    	* flumotion/ui/fgtk.py:
    	Remove all now unused widgets, add SpinButton to the list
    	of converted widgets.

--- flumotion/ui/fgtk.py
+++ flumotion/ui/fgtk.py
@@ -24,139 +24,11 @@ I am a collection of extended GTK widgets for use in Flumotion.
 """
 
 import gobject
-import gtk
 from kiwi.ui.widgets.checkbutton import ProxyCheckButton
 from kiwi.ui.widgets.combo import ProxyComboBox
-from kiwi.ui.widgets.radiobutton import ProxyRadioButton
 from kiwi.ui.widgets.entry import ProxyEntry
-
-from flumotion.common import enum, pygobject
-
-
-class FComboBox(gtk.ComboBox):
-    """
-    I am an extended combobox that can be used as a string list combobox
-    or with enums, with nick/name/value columns.
-    """
-    (COLUMN_NICK,
-     COLUMN_NAME,
-     COLUMN_VALUE) = range(3)
-
-    _column_types = str, str, int
-
-    #
-    # Public API
-    #
-
-    def get_text(self):
-        return self._get_column_content(self.COLUMN_NICK)
-
-    def get_string(self):
-        return self._get_column_content(self.COLUMN_NAME)
-
-    def get_value(self):
-        return self._get_column_content(self.COLUMN_VALUE)
-
-    def get_int(self):
-        """
-        Get name as integer.
-        """
-        s = self.get_string()
-        if s:
-            return int(s)
-        return -1
-
-    def get_enum(self):
-        # FIXME: EVIL, this should not return an integer as a fallback,
-        # because you can't call enum methods on it then
-        if hasattr(self, 'enum_class'):
-            return self.enum_class.get(self.get_value())
-        else:
-            return self.get_value()
-
-    def set_enum(self, enum_class, value_filter=()):
-        """
-        Set the given enum_class on the combobox.
-        As a side effect, this makes the combobox an enum-based one.
-        This also sets the combobox to the first enum value.
-        """
-        # throw away the old model completely
-        self._init_enum_model()
-        model = self.get_model()
-
-        for enum in enum_class:
-            # If values are specified, filter them out
-            if value_filter and not enum in value_filter:
-                continue
-            i = model.append()
-            model.set(i,
-                      self.COLUMN_NAME, enum.name,
-                      self.COLUMN_VALUE, enum.value,
-                      self.COLUMN_NICK, enum.nick)
-
-        self.set_active(0)
-        self.enum_class = enum_class
-
-    def set_list(self, list):
-        """
-        Set the given list of strings on the combobox.
-        As a side effect, turns this into a regular text combobox again.
-        """
-        if hasattr(self, 'enum_class'):
-            delattr(self, 'enum_class')
-
-        self._init_enum_model()
-        model = self.get_model()
-        for value in list:
-            i = model.append()
-            model.set(i, 0, value, 1, value)
-        self.set_active(0)
-
-    def set_multi_active(self, *values):
-        if not hasattr(self, 'enum_class'):
-            raise TypeError
-
-        self.set_enum(self.enum_class, values)
-
-    def set_active(self, item):
-        """Small wrapper around set_active() to support enums"""
-        if isinstance(item, enum.Enum):
-            gtk.ComboBox.set_active(self, item.value)
-        else:
-            gtk.ComboBox.set_active(self, item)
-
-    def get_active(self):
-        """Small wrapper around get_active() to support enums"""
-        value = gtk.ComboBox.get_active(self)
-        if hasattr(self, 'enum_class'):
-            value = self.enum_class.get(value)
-        return value
-
-    def get_state(self):
-        return self.get_enum()
-
-    #
-    # Private
-    #
-
-    def _init_enum_model(self):
-        # give ourselves a fresh enum_model
-        model = gtk.ListStore(*self._column_types)
-        self.set_model(model)
-        self.clear()
-        cell = gtk.CellRendererText()
-        self.pack_start(cell, True)
-        self.add_attribute(cell, 'text', 0)
-        return model
-
-    def _get_column_content(self, column):
-        i = self.get_active_iter()
-        if i:
-            model = self.get_model()
-            return model.get(i, column)[0]
-
-
-pygobject.type_register(FComboBox)
+from kiwi.ui.widgets.radiobutton import ProxyRadioButton
+from kiwi.ui.widgets.spinbutton import ProxySpinButton
 
 class FProxyComboBox(ProxyComboBox):
     def set_enum(self, enum_class, value_filter=()):
@@ -175,31 +47,7 @@ class FProxyComboBox(ProxyComboBox):
         self.prefill(values)
 
 
-class FEntry(gtk.Entry):
-    def get_state(self):
-        return self.get_text()
-pygobject.type_register(FEntry)
-
-class FCheckButton(gtk.CheckButton):
-    def get_state(self):
-        return self.get_active()
-pygobject.type_register(FCheckButton)
-
-class FRadioButton(gtk.RadioButton):
-    def get_state(self):
-        return self.get_active()
-
-    def __nonzero__(self):
-        return self.get_active()
-pygobject.type_register(FRadioButton)
-
-class FSpinButton(gtk.SpinButton):
-    def get_state(self):
-        return self.get_value()
-pygobject.type_register(FSpinButton)
-
-
-class WidgetMapping:
+class ProxyWidgetMapping:
     # In PyGTK 2.4.0 gtk.glade.XML type_dict parameter is buggy
     # If it can't find the name it raises a silent KeyError which
     # will be raised at random point later (as soon some code call
@@ -207,12 +55,12 @@ class WidgetMapping:
     # as it is internally, eg failback to the real GType, by doing
     # this PyMapping_GetItemString will never set the error.
 
-    types = { 'GtkCheckButton': FCheckButton,
-              'GtkComboBox': FComboBox,
-              'GtkEntry': FEntry,
-              'GtkRadioButton': FRadioButton,
-              'GtkSpinButton': FSpinButton,
-            }
+    types = { 'GtkCheckButton': ProxyCheckButton,
+              'GtkComboBox': FProxyComboBox,
+              'GtkEntry': ProxyEntry,
+              'GtkRadioButton': ProxyRadioButton,
+              'GtkSpinButton': ProxySpinButton,
+              }
 
     def __getitem__(self, name):
         if self.types.has_key(name):
@@ -221,11 +69,3 @@ class WidgetMapping:
             return gobject.type_from_name(name)
 
 
-class ProxyWidgetMapping(WidgetMapping):
-    types = WidgetMapping.types.copy()
-    types.update({
-        'GtkComboBox': FProxyComboBox,
-        'GtkCheckButton': ProxyCheckButton,
-        'GtkEntry': ProxyEntry,
-        'GtkRadioButton': ProxyRadioButton,
-        })
