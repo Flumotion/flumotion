ccadc0249d34386c1078056355da5ffbfa1b9645
Johan Dahlin <johan@flumotion.com>
2008-01-11 13:32:18 +0000

    2008-01-11  Johan Dahlin  <johan@gnome.org>
    	* common/common.mk (locale-uninstalled): Workaround intltool bug 
    	and inline the whole target to copy the .gmo files.

--- common/common.mk
+++ common/common.mk
@@ -41,10 +41,37 @@ sloc:
 .PHONY: test
 
 
-locale-uninstalled:
+locale-uninstalled-1:
 	if test -d po; then \
 	cd po; \
 	make datadir=../$(top_builddir) itlocaledir=../$(top_builddir)/locale install; \
+	fi
+
+# the locale-uninstalled rule can be replaced with the following lines, 
+# once we can depend on a newer intltool than 0.34.2
+# 	if test -d po; then \
+# 	cd po; \
+# 	make datadir=../$(top_builddir) itlocaledir=../$(top_builddir)/locale install; \
+# 	fi
+
+locale-uninstalled:
+	if test -d po; then \
+	mkdir -p $(top_builddir)/locale; \
+	cd po; \
+	make; \
+	for file in $$(ls $(srcdir)/po/*.gmo); do \
+	  lang=`echo $$file|cut -d/ -f3|cut -d. -f1`; \
+	  dir=../$(top_builddir)/locale/$$lang/LC_MESSAGES; \
+	  mkdir -p $$dir; \
+	  if test -r $$lang.gmo; then \
+	    install $$lang.gmo $$dir/$(GETTEXT_PACKAGE).mo; \
+	    echo "installing $$lang.gmo as $$dir/$(GETTEXT_PACKAGE).mo"; \
+	  else \
+	    install $(srcdir)/$$lang.gmo $$dir/$(GETTEXT_PACKAGE).mo; \
+	    echo "installing $(srcdir)/$$lang.gmo as" \
+		 "$$dir/$(GETTEXT_PACKAGE).mo"; \
+	  fi; \
+	done; \
 	fi
 
 locale-uninstalled-clean:
