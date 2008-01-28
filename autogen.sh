a24ebd6901bf70ea0a62f5b75b98acd945ef24c2
Johan Dahlin <johan@flumotion.com>
2008-01-28 16:54:47 +0000

    2008-01-28  Johan Dahlin  <johan@gnome.org>
    	* autogen.sh: Run libtoolize first, and aclocal after
    	intltoolisize, run autopoint --force, mimics the behavior
    	of gnome-autogen.sh and avoids autopoints complaints.

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -x
 
-autopoint || exit 1
-aclocal -I m4 -I common || exit 1
 libtoolize --force || exit 1
+autopoint --force || exit 1
 intltoolize --copy --force --automake || exit 1
+aclocal -I m4 -I common || exit 1
 # autoheader || exit 1
 autoconf || exit 1
 automake -a || exit 1
