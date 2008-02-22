83d03aec9bfb8ce09a8a12e80a36f0d3daa50b5f
Johan Dahlin <johan@flumotion.com>
2008-02-22 11:43:26 +0000

    2008-02-22  Johan Dahlin  <johan@gnome.org>
        * flumotion/component/misc/httpfile/admin_gtk.py: 
        Clean up, make a few methods private and organize methods by type.
        Make all internal attributes private and avoid hasattr.

--- flumotion/component/misc/httpfile/admin_gtk.py
+++ flumotion/component/misc/httpfile/admin_gtk.py
@@ -2,7 +2,7 @@
 # vi:si:et:sw=4:sts=4:ts=4
 #
 # Flumotion - a streaming media server
-# Copyright (C) 2004,2005,2006,2007 Fluendo, S.L. (www.fluendo.com).
+# Copyright (C) 2004,2005,2006,2007,2008 Fluendo, S.L. (www.fluendo.com).
 # All rights reserved.
 
 # This file may be distributed and/or modified under the terms of
@@ -20,7 +20,6 @@
 # Headers in this file shall remain intact.
 
 import os
-import gtk
 
 from gettext import gettext as _
 
@@ -32,71 +31,80 @@ __version__ = "$Rev$"
 
 class StatisticsAdminGtkNode(BaseAdminGtkNode):
     glade_file = os.path.join('flumotion', 'component', 'misc',
-        'httpfile', 'httpfile.glade')
+                              'httpfile', 'httpfile.glade')
 
     def __init__(self, *args, **kwargs):
         BaseAdminGtkNode.__init__(self, *args, **kwargs)
-        self.shown = False
+        self._shown = False
+        self._statistics = None
         self._stats = None
+        self._labels = {}
 
-    def setStats(self, stats):
-        # Set _stats regardless of if condition
-        # Used to be a race where _stats was
-        # not set if widget tree was gotten before
-        # ui state
-        self._stats = stats
-        if not hasattr(self, 'statistics'):
-            # widget tree not created yet
-            return
-
-        self.updateLabels(stats)
-
-        if not self.shown:
-            # widget tree created but not yet shown
-            self.shown = True
-            self.statistics.show_all()
-
-    def registerLabel(self, name):
-        #widgetname = name.replace('-', '_')
-        #FIXME: make object member directly
-        widget = self.wtree.get_widget('label-' + name)
-        if widget:
-            self.labels[name] = widget
-        else:
-            self.warning("FIXME: no widget %s" % name)
-
-    def hideLabels(self):
-        for name in self.labels.keys():
-            self.labels[name].hide()
-
-    def updateLabels(self, state):
-        if not hasattr(self, 'labels'):
-            return
-
-        for name in self.labels.keys():
-            text = state.get(name)
-            if text is not None:
-                if name == 'bytes-transferred':
-                    text = common.formatStorage(int(text)) + _('Byte')
-                self.labels[name].set_text(str(text))
+    # BaseAdminGtkNode
 
     def haveWidgetTree(self):
-        self.labels = {}
-        self.statistics = self.wtree.get_widget('statistics-widget')
-        self.widget = self.statistics
+        self._labels = {}
+        self._statistics = self.wtree.get_widget('statistics-widget')
+        self.widget = self._statistics
         for type in ('bytes-transferred', 'connected-clients'):
-            self.registerLabel(type)
+            self._registerLabel(type)
 
-        self.updateLabels({
+        self._updateLabels({
             'bytes-transferred': 0,
             'connected-clients': 0,
         })
         if self._stats:
             self.shown = True
-            self.updateLabels(self._stats)
-            self.statistics.show_all()
+            self._updateLabels(self._stats)
+            self._statistics.show_all()
 
-        return self.statistics
+        return self._statistics
+
+    # Public API
+
+    def setStats(self, stats):
+        """Update the state containing all information used by this
+        interface
+        @param stats:
+        @type stats: AdminComponentUIState
+        """
+        # Set _stats regardless of if condition
+        # Used to be a race where _stats was
+        # not set if widget tree was gotten before
+        # ui state
+        self._stats = stats
+
+        if not self._statistics:
+            # widget tree not created yet
+            return
+
+        self._updateLabels(stats)
+
+        if not self._shown:
+            # widget tree created but not yet shown
+            self._shown = True
+            self._statistics.show_all()
+
+    # Private
+
+    def _updateLabels(self, state):
+        for name in self._labels:
+            text = state.get(name)
+            if text is not None:
+                if name == 'bytes-transferred':
+                    text = common.formatStorage(int(text)) + _('Byte')
+                self._labels[name].set_text(str(text))
+
+    def _registerLabel(self, name):
+        #widgetname = name.replace('-', '_')
+        # FIXME: make object member directly
+        widget = self.wtree.get_widget('label-' + name)
+        if not widget:
+            self.warning("FIXME: no widget %s" % name)
+            return
+
+        self._labels[name] = widget
+
 
 class HTTPFileAdminGtk(BaseAdminGtk):
     def setup(self):
