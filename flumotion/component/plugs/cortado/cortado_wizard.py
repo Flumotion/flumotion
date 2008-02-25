0d5f04990e47de5660d36b9e208dd61740d970ed
Michael Smith <msmith@fluendo.com>
2008-02-25 11:52:03 +0000

    * flumotion/component/plugs/cortado/cortado_wizard.py:       Remove 'socket' here; no longer needed since the config writer doesn't       use it now.

--- flumotion/component/plugs/cortado/cortado_wizard.py
+++ flumotion/component/plugs/cortado/cortado_wizard.py
@@ -32,7 +32,6 @@ class CortadoHTTPPlug(HTTPPlug):
     Cortado HTTP streaming plug.
     """
     plug_type = "cortado-plug"
-    socket = "flumotion.component.plugs.cortado.cortado.CortadoPlug"
 
     # Component
 
