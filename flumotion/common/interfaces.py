9e2bd9e44e880408c29b11305bd460d44f6941ae
Johan Dahlin <johan@flumotion.com>
2008-03-04 13:12:03 +0000

    2008-03-04  Johan Dahlin  <johan@gnome.org>
        * flumotion/common/interfaces.py: General maintainance:
        - Remove self from arguments
        - add @param to missing parameters
        - Add two empty new lines between classes (PEP-8)
        - Remove initial newline in docstring (PEP-8)
        - Remove unnecessary pass statements

--- flumotion/common/interfaces.py
+++ flumotion/common/interfaces.py
@@ -2,7 +2,7 @@
 # vi:si:et:sw=4:sts=4:ts=4
 #
 # Flumotion - a streaming media server
-# Copyright (C) 2004,2005,2006,2007 Fluendo, S.L. (www.fluendo.com).
+# Copyright (C) 2004,2005,2006,2007,2008 Fluendo, S.L. (www.fluendo.com).
 # All rights reserved.
 
 # This file may be distributed and/or modified under the terms of
@@ -19,8 +19,7 @@
 
 # Headers in this file shall remain intact.
 
-"""
-Flumotion interfaces
+"""Flumotion interfaces
 """
 
 from zope.interface import Interface
@@ -30,111 +29,96 @@ __version__ = "$Rev$"
 
 # See also flumotion.medium.BaseMedium.
 class IMedium(Interface):
-    """
-    I am a base interface for PB client-side mediums interfacing with
+    """I am a base interface for PB client-side mediums interfacing with
     manager-side avatars.
     """
-
-    def setRemoteReference(self, remoteReference):
-        """
-        Set the RemoteReference to the manager-side avatar.
-
+    def setRemoteReference(remoteReference):
+        """Set the RemoteReference to the manager-side avatar.
         @param remoteReference: L{twisted.spread.pb.RemoteReference}
         """
 
-    def hasRemoteReference(self):
-        """
-        Check if we have a remote reference to the PB server's avatar.
-
+    def hasRemoteReference():
+        """Check if we have a remote reference to the PB server's avatar.
         @returns: True if we have a remote reference
         """
 
-    def callRemote(self, name, *args, **kwargs):
+    def callRemote(name, *args, **kwargs):
+        """Call a method through the remote reference to the manager-side avatar.
+        @param name: name of remote method
         """
-        Call a method through the remote reference to the manager-side avatar.
-        """
+
 
 class IComponentMedium(IMedium):
-    """
-    I am an interface for component-side mediums interfacing with server-side
+    """I am an interface for component-side mediums interfacing with server-side
     avatars.
     """
-    pass
+
 
 class IStreamingComponent(Interface):
-    """
-    An interface for streaming components, for plugs that require a streaming
+    """An interface for streaming components, for plugs that require a streaming
     component of some sort to use.
     """
-    def getUrl(self):
-        """
-        Return a URL that the streaming component is streaming.
+    def getUrl():
+        """Return a URL that the streaming component is streaming.
         """
 
-    def getDescription(self):
+    def getDescription():
+        """Return a description of the stream from this component.
         """
-        Return a description of the stream from this component.
-        """
+
 
 class IAdminMedium(IMedium):
-    """
-    I am an interface for admin-side mediums interfacing with manager-side
+    """I am an interface for admin-side mediums interfacing with manager-side
     avatars.
     """
-    pass
+
 
 class IWorkerMedium(IMedium):
-    """
-    I am an interface for worker-side mediums interfacing with manager-side
+    """I am an interface for worker-side mediums interfacing with manager-side
     avatars.
     """
-    pass
+
 
 class IPorterMedium(IMedium):
+    """I am an interface for porter client mediums interfacing with the porter.
     """
-    I am an interface for porter client mediums interfacing with the porter.
-    """
-    pass
+
 
 class IJobMedium(IMedium):
-    """
-    I am an interface for job-side mediums interfacing with worker-side
+    """I am an interface for job-side mediums interfacing with worker-side
     avatars.
     """
-    pass
+
 
 class IFeedMedium(IMedium):
-    """
-    I am an interface for mediums in a job or manager interfacing with feed
+    """I am an interface for mediums in a job or manager interfacing with feed
     avatars.
     """
-    pass
+
 
 class IHeaven(Interface):
+    """My implementors manage avatars logging in to the manager.
     """
-    My implementors manage avatars logging in to the manager.
-    """
-    def createAvatar(self, avatarId):
-        """
-        Creates a new avatar matching the type of heaven.
-
+    def createAvatar(avatarId):
+        """Creates a new avatar matching the type of heaven.
+        @param avatarId:
         @type avatarId: string
-
         @returns: the avatar from the matching heaven for a new object.
         """
 
-    def removeAvatar(self, avatarId):
-        """
-        Remove the avatar with the given Id from the heaven.
+    def removeAvatar(avatarId):
+        """Remove the avatar with the given Id from the heaven.
         """
 
+
 class IFeedServerParent(Interface):
-    """
-    I am an interface for objects that manage a FeedServer, allowing the
+    """I am an interface for objects that manage a FeedServer, allowing the
     FeedServer to hand off file descriptors to eaters and feeders managed
     by the parent.
     """
-    def feedToFD(self, componentId, feedName, fd):
-        """
-        Make the component feed the given feed to the fd.
+    def feedToFD(componentId, feedName, fd):
+        """Make the component feed the given feed to the fd.
+        @param componentId:
+        @param feedName: a feed name
+        @param fd: a file descriptor
         """
