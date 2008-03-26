c3d031529f7009c674ecef6772a520f8ed35dd6f
Zaheer Abbas Merali <zaheerabbas at merali dot org>
2008-03-26 12:23:43 +0000

    reviewed by: Johan Dahlin 	* flumotion/component/encoders/Makefile.am: 	* flumotion/component/encoders/mulaw/mulaw-wizard.glade: 	* flumotion/component/encoders/mulaw/mulaw.xml: 	* flumotion/component/encoders/mulaw/mulaw_wizard.py: 	* po/POTFILES.in: 	Create a mulaw wizard. Fixes #194.

---
+++ flumotion/component/encoders/mulaw/mulaw_wizard.py
@@ -0,0 +1,62 @@
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+#
+# Flumotion - a streaming media server
+# Copyright (C) 2004,2005,2006,2007,2008 Fluendo, S.L. (www.fluendo.com).
+# All rights reserved.
+
+# This file may be distributed and/or modified under the terms of
+# the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+# This file is distributed without any warranty; without even the implied
+# warranty of merchantability or fitness for a particular purpose.
+# See "LICENSE.GPL" in the source distribution for more information.
+
+# Licensees having purchased or holding a valid Flumotion Advanced
+# Streaming Server license may use this file in accordance with the
+# Flumotion Advanced Streaming Server Commercial License Agreement.
+# See "LICENSE.Flumotion" in the source distribution for more information.
+
+# Headers in this file shall remain intact.
+
+import gettext
+import os
+
+from zope.interface import implements
+
+from flumotion.wizard.basesteps import AudioEncoderStep
+from flumotion.wizard.interfaces import IEncoderPlugin
+from flumotion.wizard.models import AudioEncoder
+
+__version__ = "$Rev: 6359 $"
+_ = gettext.gettext
+
+
+class MulawAudioEncoder(AudioEncoder):
+    component_type = 'mulaw-encoder'
+
+
+class MulawStep(AudioEncoderStep):
+    name = _('Mulaw encoder')
+    sidebar_name = _('Mulaw')
+    glade_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                              'mulaw-wizard.glade')
+    section = _('Conversion')
+    component_type = 'mulaw-encoder'
+
+    # WizardStep
+
+    def worker_changed(self, worker):
+        self.model.worker = worker
+        self.wizard.require_elements(worker, 'mulawenc')
+
+
+class MulawWizardPlugin(object):
+    implements(IEncoderPlugin)
+    def __init__(self, wizard):
+        self.wizard = wizard
+        self.model = MulawAudioEncoder()
+
+    def getConversionStep(self):
+        return MulawStep(self.wizard, self.model)
+
