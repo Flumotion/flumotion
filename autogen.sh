e3aa1a279a266226cb69e7409cf4ad112cdbe88f
Johan Dahlin <johan@flumotion.com>
2008-04-16 16:41:15 +0000

    2008-04-16  Johan Dahlin  <johan@gnome.org>
        * autogen.sh: Copy and force installation of missing files from automake 
        and libtool.

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -x
 
-libtoolize --force || exit 1
+libtoolize -c --force || exit 1
 autopoint --force || exit 1
 cp -f common/intltool-Makefile.in.in po/Makefile.in.in
 aclocal -I m4 -I common || exit 1
 # autoheader || exit 1
 autoconf || exit 1
-automake -a || exit 1
+automake -a -c -f || exit 1
 
 echo "./autogen.sh $@" > autoregen.sh
 chmod +x autoregen.sh
