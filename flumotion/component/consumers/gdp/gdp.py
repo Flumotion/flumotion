43779a2c594fadbaca4be7a0f85319be6021a887
Thomas Vander Stichele <thomas@apestaart.org>
2008-06-20 17:13:49 +0000

    * configure.ac: 	* flumotion/component/consumers/Makefile.am: 	* flumotion/component/consumers/gdp (added): 	* flumotion/component/consumers/gdp/gdp.py (added): 	* flumotion/component/consumers/gdp/__init__.py (added): 	* flumotion/component/consumers/gdp/Makefile.am (added): 	* flumotion/component/consumers/gdp/gdp.xml (added): 	  Add a GDP consumer.

---
+++ flumotion/component/consumers/gdp/gdp.py
@@ -0,0 +1,35 @@
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+#
+# Flumotion - a streaming media server
+# Copyright (C) 2004,2005,2006,2007 Fluendo, S.L. (www.fluendo.com).
+# All rights reserved.
+
+# This file may be distributed and/or modified under the terms of
+# the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+# This file is distributed without any warranty; without even the implied
+# warranty of merchantability or fitness for a particular purpose.
+# See "LICENSE.GPL" in the source distribution for more information.
+
+# Licensees having purchased or holding a valid Flumotion Advanced
+# Streaming Server license may use this file in accordance with the
+# Flumotion Advanced Streaming Server Commercial License Agreement.
+# See "LICENSE.Flumotion" in the source distribution for more information.
+
+# Headers in this file shall remain intact.
+
+from flumotion.component import feedcomponent
+from flumotion.common import gstreamer, messages
+from flumotion.common.i18n import N_, gettexter
+
+T_ = gettexter()
+
+__version__ = "$Rev: 6125 $"
+
+
+class GDPConsumer(feedcomponent.ParseLaunchComponent):
+    def get_pipeline_string(self, properties):
+        port = properties['port']
+
+        return 'gdppay ! tcpserversink port=%d' % port
