6c3cfa335fb2582f823638d1671fbaf2fa4850f8
Thomas Vander Stichele <thomas@apestaart.org>
2008-06-28 23:21:22 +0000

    * common/epydoc.mk: 	  Try and get a hopefully unique DISPLAY number by calling getpid 	  and adding 10 (to stay out of the common 0-9 range).

--- common/epydoc.mk
+++ common/epydoc.mk
@@ -10,10 +10,12 @@ pypath = $(top_builddir):$(top_srcdir):$(PYTHONPATH)
 
 html/index.html: $(patsubst %, $(top_srcdir)/%, $(MODULE_FILES)) $(top_srcdir)/common/gendoc.py
 	@echo Generating HTML documentation...
-	Xvfb :1001 > /dev/null 2>&1 & echo $$! > Xvfb.pid
-	@DISPLAY=:1001 PYTHONPATH=$(pypath) $(PYTHON) $(top_srcdir)/common/gendoc.py $(EPYDOC_ARGS) $(MODULES) 2>&1
-	kill `cat Xvfb.pid`
-	rm Xvfb.pid
+	@$(PYTHON) -c "import os; print os.getpid() + 10" > display
+	@Xvfb -ac :`cat display` & echo $$! > Xvfb.pid
+	@DISPLAY=:`cat display` PYTHONPATH=$(pypath) $(PYTHON) $(top_srcdir)/common/gendoc.py $(EPYDOC_ARGS) $(MODULES) 2>&1; echo $$? > ret
+	@kill `cat Xvfb.pid`
+	@rm Xvfb.pid display
+	@exit `cat ret; rm -f ret`
 # FIXME: this stopped working when running with Xvfb, but no idea why
 # we need to look at newer epydoc anyway
 #| $(PYTHON) $(top_srcdir)/common/filterdoc.py
