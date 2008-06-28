e84aaebca9f37f18e9a3464bde3a2d0a3887e1cf
Thomas Vander Stichele <thomas@apestaart.org>
2008-06-28 17:55:25 +0000

    * configure.ac: 	  Check for Xvfb 	* common/epydoc.mk: 	  Newer epydoc introspects, so we run a temporary Xvfb server 	  to handle gtk imports. 	* flumotion.spec.in: 	  BuildRequire Xvfb.

--- common/epydoc.mk
+++ common/epydoc.mk
@@ -10,7 +10,13 @@ pypath = $(top_builddir):$(top_srcdir):$(PYTHONPATH)
 
 html/index.html: $(patsubst %, $(top_srcdir)/%, $(MODULE_FILES)) $(top_srcdir)/common/gendoc.py
 	@echo Generating HTML documentation...
-	@PYTHONPATH=$(pypath) $(PYTHON) $(top_srcdir)/common/gendoc.py $(EPYDOC_ARGS) $(MODULES) 2>&1 | $(PYTHON) $(top_srcdir)/common/filterdoc.py
+	Xvfb :1001 > /dev/null 2>&1 & echo $$! > Xvfb.pid
+	@DISPLAY=:1001 PYTHONPATH=$(pypath) $(PYTHON) $(top_srcdir)/common/gendoc.py $(EPYDOC_ARGS) $(MODULES) 2>&1
+	kill `cat Xvfb.pid`
+	rm Xvfb.pid
+# FIXME: this stopped working when running with Xvfb, but no idea why
+# we need to look at newer epydoc anyway
+#| $(PYTHON) $(top_srcdir)/common/filterdoc.py
 
 all-local-epydoc: html/index.html
 
