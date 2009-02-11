b0766efc4f7c16cd48091a7ac156cadc610a5f86
Xavier Queralt <xqueralt@flumotion.com>
2009-02-11 17:51:02 +0000

    * common/rpm.mk: 	Force deletion of rpm with -f otherwise the build fails. 	Use the PACKAGE variable for the name of the spec file this 	way other packages can use the same code for building rpm.

--- common/rpm.mk
+++ common/rpm.mk
@@ -1,8 +1,8 @@
 rpm: dist
-	rm *.rpm
+	rm -f *.rpm
 	for i in RPMS SRPMS SOURCES SPECS BUILD; do mkdir -p rpmbuild/$$i; done
 	cp flumotion-*.tar.bz2 rpmbuild/SOURCES
-	rpmbuild --define "_topdir `pwd`/rpmbuild" -ba flumotion.spec
+	rpmbuild --nodeps --define "_topdir `pwd`/rpmbuild" -ba $(PACKAGE).spec
 	mv rpmbuild/RPMS/*/flumotion*.rpm .
 	mv rpmbuild/SRPMS/flumotion*.rpm .
 	rm -rf rpmbuild
