e38b6a59f16912a31fa53c1964b910b36632c4be
Jan Urbański <wulczer@wulczer.org>
2009-10-06 11:26:31 +0000

    reviewed by: Sebastien Merle 	* flumotion/admin/connections.py: 	Implement default connection user and password file. The file looked 	for is named 'connections' and should be located in a location 	confirming to the XDG Base Directory spec. Usually, that means 	$HOME/.local/flumotion/connections. 	The format is an XML file containing a <connections> element, followed 	by a list of <connection> elements, that should be similar to the 	format of recent connection files, but can contain shell globs in the 	'host', 'port', 'use_insecure' and 'user' fields. 	Closes #1373. 	* flumotion/test/test_admin_connections.py: 	Extend the existing tests for recent connections and test the new 	default connections functionality.

--- flumotion/admin/connections.py
+++ flumotion/admin/connections.py
@@ -22,10 +22,11 @@
 """recent connections"""
 
 import datetime
+import fnmatch
 import os
 from xml.dom import minidom, Node
 
-from flumotion.common import log, common
+from flumotion.common import log, common, xdg
 from flumotion.common.connection import PBConnectionInfo, parsePBConnectionInfo
 from flumotion.common.errors import OptionError
 # FIXME: make pychecker able to suppress shadowed builtins like these
@@ -38,6 +39,37 @@ from flumotion.configure import configure
 from flumotion.twisted.pb import Authenticator
 
 __version__ = "$Rev$"
+
+
+class ConnectionInfo(object):
+    """
+    I wrap the information contained in a single connection file entry.
+
+    I can be used to construct L{PBConnectionInfo} object, but because some of
+    my variables can be shell globs, they are all strings.
+    """
+
+    def __init__(self, host, port, use_insecure, user, passwd):
+        self.host = host
+        self.port = port
+        self.use_insecure = use_insecure
+        self.user = user
+        self.passwd = passwd
+
+    def asPBConnectionInfo(self):
+        """
+        Return a L{PBConnectionInfo} object constructed from my state. If my
+        state contains shell globs, I might throw a ValueError.
+        """
+        if ('*' in self.host) or (self.use_insecure not in ('0', '1')):
+            raise ValueError("Shell glob in connection info")
+        return PBConnectionInfo(self.host, int(self.port),
+                                self.use_insecure == '0',
+                                Authenticator(username=self.user,
+                                              password=self.passwd))
+
+    def __str__(self):
+        return '%s@%s:%s' % (self.user, self.host, self.port)
 
 
 class RecentConnection(object):
@@ -53,7 +85,7 @@ class RecentConnection(object):
     @ivar filename:  filename of the connection
     @type filename:  string
     @ivar info:      connection info
-    @type info:      L{PBConnectionInfo}
+    @type info:      L{ConnectionInfo}
     @ivar timestamp: timestamp
     @type timestamp: datetime.datetime
     """
@@ -95,6 +127,26 @@ def hasRecentConnections():
     return True
 
 
+def _parseConnection(element):
+    state = {}
+    for childNode in element.childNodes:
+        if (childNode.nodeType != Node.TEXT_NODE and
+            childNode.nodeType != Node.COMMENT_NODE):
+            state[childNode.nodeName] = childNode.childNodes[0].wholeText
+    return ConnectionInfo(state['host'], state['port'], state['use_insecure'],
+                          state['user'], state['passwd'])
+
+
+def _parseSingleConnectionFile(filename):
+    tree = minidom.parse(filename)
+    return _parseConnection(tree.documentElement)
+
+
+def _parseMultipleConnectionsFile(filename):
+    tree = minidom.parse(filename)
+    return map(_parseConnection, tree.getElementsByTagName('connection'))
+
+
 def getRecentConnections():
     """
     Fetches a list of recently used connections
@@ -102,26 +154,11 @@ def getRecentConnections():
     @rtype: list of L{RecentConnection}
     """
 
-    def _parseConnection(filename):
-        tree = minidom.parse(filename)
-        state = {}
-        for childNode in tree.documentElement.childNodes:
-            if (childNode.nodeType != Node.TEXT_NODE and
-                childNode.nodeType != Node.COMMENT_NODE):
-                state[childNode.nodeName] = childNode.childNodes[0].wholeText
-        state['port'] = int(state['port'])
-        state['use_insecure'] = (state['use_insecure'] != '0')
-        authenticator = Authenticator(username=state['user'],
-                                      password=state['passwd'])
-        return PBConnectionInfo(state['host'], state['port'],
-                                not state['use_insecure'],
-                                authenticator)
-
     recentFilenames = _getRecentFilenames()
     recentConnections = []
     for filename in sorted(recentFilenames, reverse=True):
         try:
-            state = _parseConnection(filename)
+            state = _parseSingleConnectionFile(filename)
             recentConnections.append(
                 RecentConnection(str(state),
                                  filename=filename,
@@ -131,11 +168,80 @@ def getRecentConnections():
     return recentConnections
 
 
+def getDefaultConnections():
+    """
+    Fetches a list of default connections.
+
+    @returns: default connections
+    @rtype: list of L{PBDefaultConnectionInfo}
+    """
+
+    filename = xdg.config_read_path('connections')
+    if not filename:
+        return []
+
+    try:
+        return _parseMultipleConnectionsFile(filename)
+    except Exception, e:
+        log.warning('connections', 'Error parsing %s: %r', filename, e)
+    return []
+
+
+def updateFromConnectionList(info, connections, match_glob=False):
+    """
+    Updates the info object with the username and password taken from the list
+    of connections.
+
+    @param info:        connection info
+    @type info:         L{PBConnectionInfo}
+    @param connections: recent or default connections
+    @type:              a list of L{PBConnectionInfo}
+    @param match_glob:  if values of host, port, etc. to be matched between
+                        info and the recent or default connections should be
+                        treated as shell globs
+    @type:              boolean
+    @returns:           None
+    """
+
+    def match(v1, v2):
+        if match_glob:
+            # v2 is the candidate, which might be a shell glob
+            return fnmatch.fnmatch(v1, v2)
+        else:
+            return v1 == v2
+
+    def compatible(info, c_info):
+        if not match(info.host, c_info.host):
+            return False
+        port = str(info.port)
+        if not match(port, c_info.port):
+            return False
+        use_insecure = info.use_ssl and '0' or '1'
+        if not match(use_insecure, c_info.use_insecure):
+            return False
+        auth = info.authenticator
+        if auth.username and not match(auth.username, c_info.user):
+            return False
+        # doesn't make sense to match the password, if everything before that
+        # matched, we won't fill in anything
+        return True
+
+    for candidate in connections:
+        if compatible(info, candidate):
+            # it's compatible, fill in the variables
+            if not info.authenticator.username:
+                info.authenticator.username = candidate.user
+            if not info.authenticator.password:
+                info.authenticator.password = candidate.passwd
+            break
+    return info
+
+
 def parsePBConnectionInfoRecent(managerString, use_ssl=True,
                                 defaultPort=configure.defaultSSLManagerPort):
     """The same as L{flumotion.common.connection.parsePBConnectionInfo},
-    but fills in missing information from the recent connections cache
-    if possible.
+    but fills in missing information from the recent connections cache or
+    from the default user and password definitions file if possible.
     @param managerString: manager string we should connect to
     @type managerString: string
     @param use_ssl: True if we should use ssl
@@ -148,7 +254,7 @@ def parsePBConnectionInfoRecent(managerString, use_ssl=True,
     recent = getRecentConnections()
     if not managerString:
         if recent:
-            return recent[0].info
+            return recent[0].info.asPBConnectionInfo()
         else:
             raise OptionError('No string given and no recent '
                               'connections to use')
@@ -158,31 +264,12 @@ def parsePBConnectionInfoRecent(managerString, use_ssl=True,
                                  port=defaultPort,
                                  use_ssl=use_ssl)
 
-    def compatible(i1, i2):
-        if i1.port and i1.port != i2.port:
-            return False
-        if i1.use_ssl != i2.use_ssl:
-            return False
-        a1, a2 = i1.authenticator, i2.authenticator
-        if a1.username and a1.username != a2.username:
-            return False
-        if a1.password and a1.password != a2.password:
-            return False
-        return True
-
-    if not info.authenticator.username:
-        for c in recent:
-            recent = c.info
-            if compatible(info, recent):
-                info.authenticator.username = recent.authenticator.username
-                info.authenticator.password = recent.authenticator.password
-                break
-    elif not info.authenticator.password:
-        for c in recent:
-            recent = c.info
-            if compatible(info, recent):
-                info.authenticator.password = recent.authenticator.password
-                break
+    if not (info.authenticator.username and info.authenticator.password):
+        recent_infos = [r.info for r in recent]
+        updateFromConnectionList(info, recent_infos, match_glob=False)
+    if not (info.authenticator.username and info.authenticator.password):
+        defaults = getDefaultConnections()
+        updateFromConnectionList(info, defaults, match_glob=True)
     if not (info.authenticator.username and info.authenticator.password):
         raise OptionError('You are connecting to %s for the '
                           'first time; please specify a user and '
--- flumotion/test/test_admin_connections.py
+++ flumotion/test/test_admin_connections.py
@@ -21,14 +21,145 @@
 
 from flumotion.common import testsuite
 
-from flumotion.admin.connections import getRecentConnections, \
-     RecentConnection
+import os
+import shutil
+from flumotion.configure import configure
+from flumotion.common import connection, errors, xdg
+from flumotion.admin.connections import getRecentConnections
+from flumotion.admin.connections import parsePBConnectionInfoRecent
+
+
+def _create_connection(f, port, host, use_insecure, user, passwd):
+    f.write('''<connection>
+                 <port>%s</port>
+                 <host>%s</host>
+                 <use_insecure>%s</use_insecure>
+                 <user>%s</user>
+                 <passwd>%s</passwd>
+               </connection>''' % (port, host, use_insecure,
+                                   user, passwd))
 
 
 class AdminConnectiontionsTest(testsuite.TestCase):
 
+    def setUp(self):
+        # create a fake list of recent connection files
+        self.old_registrydir = configure.registrydir
+        self.new_registrydir = self.mktemp()
+        os.mkdir(self.new_registrydir)
+        configure.registrydir = self.new_registrydir
+
+        rc1 = file(os.path.join(self.new_registrydir, 'fake.connection'), 'w')
+        _create_connection(rc1, 1234, 'test.host.com',
+                                  '1', 'testuser', 'testpasswd')
+        rc1.close()
+        rc2 = file(os.path.join(self.new_registrydir, 'fake2.connection'), 'w')
+        _create_connection(rc2, 1235, 'test2.host.com',
+                                  '0', 'test2user', 'test2passwd')
+        rc2.close()
+
+        # this should not be picked up as a recent connection file
+        nrc = file(os.path.join(self.new_registrydir, 'fake3'), 'w')
+        _create_connection(nrc, 1236, 'testx.host.com',
+                                  '1', 'testxuser', 'testxpasswd')
+        nrc.close()
+
+        # create a fake default connections file
+        self.old_xdg_config_home = os.environ.get('XDG_CONFIG_HOME')
+        self.new_xdg_config_home = self.mktemp()
+        os.mkdir(self.new_xdg_config_home)
+        os.environ['XDG_CONFIG_HOME'] = self.new_xdg_config_home
+
+        def1 = xdg.config_write_path('connections', 'w')
+        def1.write('<connections>')
+        _create_connection(def1, '2*', 'test3.host.com',
+                                  '1', 'testdefuser', 'testdefpasswd')
+        _create_connection(def1, '*', '*.host',
+                                  '*', 'x', 'testxpasswd')
+        def1.write('</connections>')
+        def1.close()
+
+    def tearDown(self):
+        shutil.rmtree(self.new_registrydir)
+        configure.registrydir = self.old_registrydir
+
+        shutil.rmtree(self.new_xdg_config_home)
+        if self.old_xdg_config_home is not None:
+            os.environ['XDG_CONFIG_HOME'] = self.old_xdg_config_home
+        else:
+            del os.environ['XDG_CONFIG_HOME']
+
     def testGetRecentConnections(self):
-        recent = getRecentConnections()
-        if not recent:
-            return
-        assert isinstance(recent[0], RecentConnection)
+        r = getRecentConnections()
+        self.assertEquals(len(r), 2)
+
+        # the recent connections are read in reverse lexicographical order
+        ci2, ci1 = r[0].info, r[1].info
+        self.assertEquals(ci1.port, '1234')
+        self.assertEquals(ci1.host, 'test.host.com')
+        self.assertEquals(ci1.use_insecure, '1')
+        self.assertEquals(ci1.user, 'testuser')
+        self.assertEquals(ci1.passwd, 'testpasswd')
+        self.assertEquals(ci2.port, '1235')
+        self.assertEquals(ci2.host, 'test2.host.com')
+        self.assertEquals(ci2.use_insecure, '0')
+        self.assertEquals(ci2.user, 'test2user')
+        self.assertEquals(ci2.passwd, 'test2passwd')
+
+    def testParsePBConnectionRecent(self):
+        pPBCIR = parsePBConnectionInfoRecent
+
+        info = pPBCIR('')
+        # with an empty manager string we should get the last recent connection
+        self.assertEquals(info.port, 1235)
+        self.assertEquals(info.host, 'test2.host.com')
+        self.assertEquals(info.use_ssl, True)
+        self.assertEquals(info.authenticator.username, 'test2user')
+        self.assertEquals(info.authenticator.password, 'test2passwd')
+
+        info = pPBCIR('test2.host.com:1235')
+        # there is a recent connection for this manager
+        self.assertEquals(info.port, 1235)
+        self.assertEquals(info.host, 'test2.host.com')
+        self.assertEquals(info.use_ssl, True)
+        self.assertEquals(info.authenticator.username, 'test2user')
+        self.assertEquals(info.authenticator.password, 'test2passwd')
+
+        info = pPBCIR('test.host.com:1234', use_ssl=False)
+        self.assertEquals(info.port, 1234)
+        self.assertEquals(info.host, 'test.host.com')
+        self.assertEquals(info.use_ssl, False)
+        self.assertEquals(info.authenticator.username, 'testuser')
+        self.assertEquals(info.authenticator.password, 'testpasswd')
+
+        info = pPBCIR('testuser@test.host.com:1234', use_ssl=False)
+        self.assertEquals(info.port, 1234)
+        self.assertEquals(info.host, 'test.host.com')
+        self.assertEquals(info.use_ssl, False)
+        self.assertEquals(info.authenticator.username, 'testuser')
+        self.assertEquals(info.authenticator.password, 'testpasswd')
+
+        # default connections
+        info = pPBCIR('test3.host.com:2234', use_ssl=False)
+        self.assertEquals(info.port, 2234)
+        self.assertEquals(info.host, 'test3.host.com')
+        self.assertEquals(info.use_ssl, False)
+        self.assertEquals(info.authenticator.username, 'testdefuser')
+        self.assertEquals(info.authenticator.password, 'testdefpasswd')
+
+        info = pPBCIR('x@random.host:1234')
+        self.assertEquals(info.port, 1234)
+        self.assertEquals(info.host, 'random.host')
+        self.assertEquals(info.use_ssl, True)
+        self.assertEquals(info.authenticator.username, 'x')
+        self.assertEquals(info.authenticator.password, 'testxpasswd')
+
+        # incompatible port
+        self.assertRaises(errors.OptionError, pPBCIR, 'test2.host.com:1234')
+        self.assertRaises(errors.OptionError, pPBCIR, 'test3.host.com:1234')
+        # incompatible SSL settings
+        self.assertRaises(errors.OptionError, pPBCIR, 'test.host.com:1234')
+        self.assertRaises(errors.OptionError, pPBCIR, 'test3.host.com:2234',
+                          use_ssl=True)
+        # incompatible user
+        self.assertRaises(errors.OptionError, pPBCIR, 'y@test3.host.com:1234')
