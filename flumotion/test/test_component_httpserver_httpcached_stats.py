0efce47317acfe526b99b45718f54a949ee34bc5
Xavier Queralt <xqueralt@flumotion.com>
2009-11-25 17:29:18 +0000

    The same in r8404 for f.t.test_component_httpserver_httpcached_stats

--- flumotion/test/test_component_httpserver_httpcached_stats.py
+++ flumotion/test/test_component_httpserver_httpcached_stats.py
@@ -28,7 +28,7 @@ from twisted.internet import reactor, defer
 from twisted.trial import unittest
 
 import twisted.copyright
-if twisted.copyright.version.split('.') <= ['2', '0', '1']:
+if twisted.copyright.version == "SVN-Trunk":
     SKIP_MSG = "Twisted 2.0.1 thread pool is broken for tests"
 else:
     SKIP_MSG = None
