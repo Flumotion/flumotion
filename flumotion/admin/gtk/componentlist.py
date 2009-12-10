fc6f9a365effbf760282c35ccc058a76f8f92fd5
Thomas Vander Stichele <thomas@apestaart.org>
2009-12-10 21:52:15 +0000

    reviewed by: Zaheer 	* tests/ComponentsView.py (deleted): 	  Remove old test that wasn't working anymore. 	* flumotion/admin/gtk/componentlist.py: 	  Migrate and update that code into the file straight away to have 	  a better chance at getting updated.

--- flumotion/admin/gtk/componentlist.py
+++ flumotion/admin/gtk/componentlist.py
@@ -484,3 +484,68 @@ class ComponentList(log.Loggable, gobject.GObject):
 
 
 gobject.type_register(ComponentList)
+
+
+# this file can be run to test ComponentList
+if __name__ == '__main__':
+
+    from twisted.internet import reactor
+    from twisted.spread import jelly
+
+    class Main:
+
+        def __init__(self):
+            self.window = gtk.Window()
+            self.widget = gtk.TreeView()
+            self.window.add(self.widget)
+            self.window.show_all()
+            self.view = ComponentList(self.widget)
+            self.view.connect('selection-changed', self._selection_changed_cb)
+            #self.view.connect('activated', self._activated_cb)
+            self.window.connect('destroy', gtk.main_quit)
+
+        def _createComponent(self, dict):
+            mstate = planet.ManagerComponentState()
+            for key in dict.keys():
+                mstate.set(key, dict[key])
+            astate = jelly.unjelly(jelly.jelly(mstate))
+            return astate
+
+        def tearDown(self):
+            self.window.destroy()
+
+        def update(self):
+            components = {}
+            c = self._createComponent(
+                {'name': 'one', 'mood': moods.happy.value,
+                 'workerName': 'R2D2', 'pid': 1, 'type': 'dummy'})
+            components['one'] = c
+            c = self._createComponent(
+                {'name': 'two', 'mood': moods.sad.value,
+                 'workerName': 'R2D2', 'pid': 2, 'type': 'dummy'})
+            components['two'] = c
+            c = self._createComponent(
+                {'name': 'three', 'mood': moods.hungry.value,
+                 'workerName': 'C3PO', 'pid': 3, 'type': 'dummy'})
+            components['three'] = c
+            c = self._createComponent(
+                {'name': 'four', 'mood': moods.sleeping.value,
+                 'workerName': 'C3PO', 'pid': None, 'type': 'dummy'})
+            components['four'] = c
+            self.view.clearAndRebuild(components)
+
+        def _selection_changed_cb(self, view, states):
+            # states: list of AdminComponentState
+            print "Selected component(s) %s" % ", ".join(
+                [s.get('name') for s in states])
+
+        def _activated_cb(self, view, state, action_name):
+            name = state.get('name')
+            print "Do action %s on component %s" % (action_name, name)
+
+
+    app = Main()
+
+    app.update()
+
+    gtk.main()
