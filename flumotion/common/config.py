15449f1eac923fb71072ee1786f2e45559284117
Xavier Queralt <xqueralt@flumotion.com>
2010-07-27 19:49:04 +0000

    * flumotion/common/config.py: 	Reintroduce the removed (in r8746) "socket" attribute to make 	Julien happy this time.

--- flumotion/common/config.py
+++ flumotion/common/config.py
@@ -241,7 +241,16 @@ class BaseConfigParser(fxml.Parser):
         def parsePlug(node):
             # <plug type=...>
             #   <property>
-            plugType = self.parseAttributes(node, required=('type', ))[0]
+            # F0.8
+            # socket is unneeded and deprecated; we don't use it.
+            plugType, socket = self.parseAttributes(
+                node, ('type', ), ('socket', ))
+            if socket is not None:
+                msg = ('"socket" attribute of plug tag is not used'
+                       ' and has been deprecated, please update your'
+                       ' configuration file (found offending plug of type'
+                       ' %r)' % plugType)
+                warnings.warn(msg, DeprecationWarning)
             properties = []
             parsers = {'property': (self._parseProperty, properties.append),
                        'compound-property': (self._parseCompoundProperty,
