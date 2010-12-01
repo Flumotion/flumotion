1783ce2ec3e88bbeed5633c0de99b175c25d407b
Andoni Morales Alastruey <amorales@flumotion.com>
2010-12-01 10:55:33 +0000

    reviewed by: Xavier Queralt 	* flumotion/component/consumers/disker/disker.py: 	* flumotion/test/test_component_disker.py: 	Add an index class and unit tests

--- flumotion/component/consumers/disker/disker.py
+++ flumotion/component/consumers/disker/disker.py
@@ -24,6 +24,7 @@ import os
 import time
 import tempfile
 import datetime as dt
+import bisect
 
 import gobject
 import gst
@@ -31,7 +32,8 @@ import gst
 from twisted.internet import reactor
 
 from flumotion.component import feedcomponent
-from flumotion.common import log, gstreamer, pygobject, messages, errors
+from flumotion.common import log, gstreamer, pygobject, messages,\
+                             errors, common
 from flumotion.common import documentation, format
 from flumotion.common import eventcalendar, poller
 from flumotion.common.i18n import N_, gettexter
@@ -73,6 +75,281 @@ the ical event. In practice this will either be UTC or the local time of
 the machine running the disker, as the ical scheduler does not
 understand arbitrary timezones.
 """
+
+
+def _openFile(component, location, mode):
+    try:
+        file = open(location, mode)
+        return file
+    except IOError, e:
+        component.warning("Failed to open output file %s: %s",
+                   location, log.getExceptionMessage(e))
+        m = messages.Error(T_(N_(
+            "Failed to open output file '%s' for writing. "
+            "Check permissions on the file."), location))
+        component.addMessage(m)
+        return None
+
+
+class Index(log.Loggable):
+    '''
+    Creates an index of keyframes for a file, than can be used later for
+    seeking in non indexed formats or whithout parsing the headers.
+
+    The format of the index is very similar to the AVI Index, but it can also
+    include information about the real time of each entry in UNIX time.
+    (see 'man aviindex')
+
+    If the index is for an indexed format, the offset of the first entry will
+    not start from 0. This offset is the size of the headers.  '''
+
+    # CHK:      Chunk number starting from 0
+    # POS:      Absolute byte position of the chunk in the file
+    # LEN:      Length in bytes of the chunk
+    # TS:       Timestamp of the chunk (ns)
+    # DUR:      Duration of the chunk (ns)
+    # KF:       Whether it starts with a keyframe or not
+    # TDT:      Time and date using a UNIX timestamp (s)
+    # TDUR:     Duration of the chunk in UNIX time (s)
+    INDEX_HEADER = "FLUIDX1 #Flumotion\n"
+    INDEX_KEYS = ['CHK', 'POS', 'LEN', 'TS', 'DUR', 'KF', 'TDT', 'TDUR']
+    INDEX_EXTENSION = 'index'
+
+    def __init__(self):
+        self._index = []
+        self._headers_size = 0
+
+    ### Public methods ###
+
+    def updateStart(self, timestamp):
+        '''
+        Remove entries in the index older than this timestamp
+        '''
+        self.debug("Removing entries older than %s", timestamp)
+        self._index = self._filter_index(timestamp) or []
+
+    def addEntry(self, offset, timestamp, keyframe, tdt=0):
+        '''
+        Add a new entry to the the index
+        '''
+        if len(self._index) > 0:
+            # Check that new entries have increasing timestamp, offset and tdt
+            if not self._checkEntriesContinuity(offset, timestamp, tdt):
+                return
+            # And update the length and duration of the last entry
+            self._updateLastEntry(offset, timestamp, tdt)
+
+        self._index.append({'offset': offset, 'length': -1,
+                            'timestamp': timestamp, 'duration': -1,
+                            'keyframe': keyframe, 'tdt': tdt,
+                            'tdt-duration': -1})
+
+        self.debug("Added new entry to the index: offset=%s timestamp=%s "
+                   "keyframe=%s tdt=%s", offset, timestamp, keyframe, tdt)
+
+    def setHeadersSize(self, size):
+        '''
+        Set the headers size in bytes. Multifdsink append the stream headers
+        to each client. This size is then used to adjust the offset of the
+        index entries
+        '''
+        self._headers_size = size
+
+    def getHeaders(self):
+        '''
+        Return an index entry corresponding to the headers, which is a chunk
+        with 'offset' 0 and 'length' equals to the headers size
+        '''
+        if self._headers_size == 0:
+            return None
+        return {'offset': 0, 'length': self._headers_size,
+                'timestamp': 0, 'duration': -1,
+                'keyframe': 0, 'tdt': 0, 'tdt-duration': -1}
+
+    def getFirstTimestamp(self):
+        if len(self._index) == 0:
+            return -1
+        return self._index[0]['timestamp']
+
+    def getFirstTDT(self):
+        if len(self._index) == 0:
+            return -1
+        return self._index[0]['tdt']
+
+    def clipTimestamp(self, start, stop):
+        '''
+        Clip the current index to a start and stop time, returning all the
+        entries matching the boundaries using the 'timestamp'
+        '''
+        return self._clip('timestamp', 'duration', start, stop)
+
+    def clipTDT(self, start, stop):
+        '''
+        Clip the current index to a start and stop time, returning all the
+        entries matching the boundaries using the 'tdt'
+        '''
+        return self._clip('tdt', 'tdt-duration', start, stop)
+
+    def clear(self):
+        '''
+        Clears the index
+        '''
+        self._index = []
+
+    def save(self, location, start=None, stop=None):
+        '''
+        Saves the index in a file, using the entries from 'start' to 'stop'
+        '''
+        if len(self._index) == 0:
+            self.warning("The index doesn't contain any entry and it will not "
+                         "be saved")
+            return False
+        f = _openFile(self, location, 'w+')
+        if not f:
+            return False
+
+        self._write_index_headers(f)
+        self._write_index_entries(f, self._filter_index(start, stop))
+        self.info("Index saved successfully. start=%s stop=%s location=%s ",
+                   start, stop, location)
+        return True
+
+    def loadIndexFile(self, location):
+        '''
+        Loads the entries of the index from an index file
+        '''
+
+        def invalidIndex(reason):
+            self.warning("This file is not a valid index: %s", reason)
+            return False
+
+        if not location.endswith(self.INDEX_EXTENSION):
+            return invalidIndex("the extension of this file is not '%s'" %
+                                self.INDEX_EXTENSION)
+        try:
+            self.info("Loading index file %s", location)
+            file = open(location, 'r')
+            indexString = file.readlines()
+            file.close()
+        except IOError, e:
+            return invalidIndex("error reading index file (%r)" % e)
+        # Check if the file is not empty
+        if len(indexString) == 0:
+            return invalidIndex("the file is empty")
+        # Check headers
+        if not indexString[0].startswith('FLUIDX1 #'):
+            return invalidIndex('header is not FLUIDX1')
+        # Check index keys declaration
+        keysStr = ' '.join(self.INDEX_KEYS)
+        if indexString[1].strip('\n') != keysStr:
+            return invalidIndex('keys definition is not: %s' % keysStr)
+        # Add entries
+        setHeaders = True
+        for entryLine in indexString[2:]:
+            e = entryLine.split(' ')
+            if len(e) < len(self.INDEX_KEYS):
+                return invalidIndex("one of the entries doesn't have enough "
+                                    "parameters (needed=%d, provided=%d)" %
+                                    (len(self.INDEX_KEYS), len(e)))
+            try:
+                self.addEntry(int(e[1]), int(e[3]), common.strToBool(e[5]),
+                              int(e[6]))
+            except Exception, e:
+                return invalidIndex("could not parse one of the entries: %r"
+                                    % e)
+            if setHeaders:
+                self._headers_size = int(e[1])
+                setHeaders = False
+        self.info("Index parsed successfully")
+        return True
+
+    ### Private methods ###
+
+    def _updateLastEntry(self, offset, timestamp, tdt):
+        last = self._index[-1]
+        last['length'] = offset - last['offset']
+        last['duration'] = timestamp - last['timestamp']
+        last['tdt-duration'] = tdt - last['tdt']
+
+    def _checkEntriesContinuity(self, offset, timestamp, tdt):
+        last = self._index[-1]
+        for key, value in [('offset', offset), ('timestamp', timestamp),
+                           ('tdt', tdt)]:
+            if value < last[key]:
+                self.warning("Could not add entries with a decreasing %s "
+                         "(last=%s, new=%s)", key, last[key], value)
+                return False
+        return True
+
+    def _clip(self, keyTS, keyDur, start, stop):
+        '''
+        Clip the index to a start and stop time. For an index with 10
+        entries of 10 seconds starting from 0, cliping from 15 to 35 will
+        return the entries 1, 2, and 3.
+        '''
+        if start >= stop:
+            return None
+
+        keys = [e[keyTS] for e in self._index]
+
+        # If the last entry has a duration we add a new entry in the TS list
+        # with the stop time
+        lastEntry = self._index[-1]
+        if lastEntry[keyDur] != -1:
+            keys.append(lastEntry[keyTS] + lastEntry[keyDur])
+
+        # Return if the start and stop time are not inside the boundaries
+        if stop <= keys[0] or start >= keys[-1]:
+            return None
+
+        # Set the start and stop time to match the boundaries so that we don't
+        # get indexes outside the array boundaries
+        if start <= keys[0]:
+            start = keys[0]
+        if stop >= keys[-1]:
+            stop = keys[-1] - 1
+
+        # Do the bisection
+        i_start = bisect.bisect_right(keys, start) - 1
+        i_stop = bisect.bisect_right(keys, stop)
+
+        return self._index[i_start:i_stop]
+
+    def _filter_index(self, start=None, stop=None):
+        '''
+        Filter the index with a start and stop time.
+        FIXME: Check performance difference with clipping
+        '''
+        if len(self._index) == 0:
+            return
+        if not start and not stop:
+            return self._index
+        if not start:
+            start = self._index[0]['timestamp']
+        if not stop:
+            last_entry = self._index[len(self._index)-1]
+            stop = last_entry['timestamp'] + 1
+        return [x for x in self._index if (x['timestamp'] >= start and\
+                x['timestamp'] <= stop)]
+
+    def _write_index_headers(self, file):
+        file.write("%s" % self.INDEX_HEADER)
+        file.write("%s\n" % ' '.join(self.INDEX_KEYS))
+
+    def _write_index_entries(self, file, entries):
+        offset = self._headers_size - self._index[0]['offset']
+        count = 0
+
+        for entry in self._index:
+            frmt = "%s\n" % " ".join(['%s'] * len(self.INDEX_KEYS))
+            file.write(frmt % (count, entry['offset'] + offset,
+                                      entry['length'],
+                                      entry['timestamp'],
+                                      entry['duration'],
+                                      entry['keyframe'],
+                                      entry['tdt'],
+                                      entry['tdt-duration']))
+            count += 1
 
 
 class DiskerMedium(feedcomponent.FeedComponentMedium):
--- flumotion/test/test_component_disker.py
+++ flumotion/test/test_component_disker.py
@@ -19,12 +19,11 @@
 
 # Headers in this file shall remain intact.
 
-from twisted.python import failure
-from twisted.internet import defer, reactor, interfaces, gtk2reactor
-from twisted.web import client, error
+import tempfile
+import os
 
 from flumotion.common import testsuite
-from flumotion.common import log, errors
+from flumotion.common import log
 from flumotion.common.planet import moods
 from flumotion.component.consumers.disker import disker
 
@@ -198,3 +197,235 @@ class TestFlow(PluggableComponentTestCase, log.Loggable):
         # and finally stop the flow
         d.addCallback(lambda _: self.tp.stop_flow())
         return d
+
+
+class FakeComponent:
+
+    def warning(self, a, b, c):
+        pass
+
+    def addMessage(self, a):
+        pass
+
+
+def warning(*args):
+    if len(args) == 0:
+        message = ''
+    elif len(args) == 1:
+        message = args[0]
+    if len(args) > 1:
+        message = args[0] % args[1:]
+    raise Exception(message)
+
+
+class TestIndex(testsuite.TestCase):
+
+    INDEX = '''\
+%s
+CHK POS LEN TS DUR KF %s TDUR
+0 0 1 10 10 True 110 10
+1 1 1 20 10 True%s 10
+2 2 -1 30 -1 True 130 -1'''
+
+    def setUp(self):
+        self.component = FakeComponent()
+        self.index = disker.Index()
+        self.index.warning = warning
+
+    def failUnlessRaisesWithMessage(self, message, call, *args):
+        try:
+            call(*args)
+        except Exception, e:
+            self.failIf(not str(e).startswith(message))
+            return
+        self.fail()
+
+    def checkEntry(self, entry, offset, timestamp):
+        self.assertEquals(entry['offset'], offset)
+        self.assertEquals(entry['timestamp'], timestamp)
+
+    def fillIndex(self):
+        self.index.addEntry(0, 10, 1, 110)
+        self.index.addEntry(1, 20, 1, 120)
+        self.index.addEntry(2, 30, 1, 130)
+
+    def fillBigIndex(self):
+        for i in range(100):
+            self.index.addEntry(i, i*10, 1, 0)
+
+    def testAddIndexEntry(self):
+        self.index.addEntry(0, 10, 1, 0)
+        self.checkEntry(self.index._index[0], 0, 10)
+
+    def testAddMultipleEntries(self):
+        self.fillIndex()
+        self.checkEntry(self.index._index[0], 0, 10)
+        self.checkEntry(self.index._index[1], 1, 20)
+        self.checkEntry(self.index._index[2], 2, 30)
+
+    def testAddNonIncreasingEntry(self):
+        self.index.addEntry(0, 10, 1, 10)
+        self.index.addEntry(1, 20, 1, 20)
+        self.failUnlessRaisesWithMessage("Could not add entries with a "
+            "decreasing timestamp", self.index.addEntry, 2, 10, 1, 30)
+
+    def testClearIndex(self):
+        self.fillIndex()
+        self.index.clear()
+        self.assertEquals(len(self.index._index), 0)
+
+    def testUpdateStart(self):
+        self.fillIndex()
+        self.index.updateStart(11)
+        self.assertEquals(len(self.index._index), 2)
+        self.checkEntry(self.index._index[0], 1, 20)
+        self.checkEntry(self.index._index[1], 2, 30)
+
+    def testSave(self):
+        self.fillIndex()
+        fd, path = tempfile.mkstemp()
+        ret = self.index.save(path, 0)
+        self.assertEquals(ret, True)
+        file = open(path, 'r')
+        lines = file.readlines()
+        self.assertEquals(lines,
+            ['FLUIDX1 #Flumotion\n',
+            'CHK POS LEN TS DUR KF TDT TDUR\n',
+            '0 0 1 10 10 1 110 10\n',
+            '1 1 1 20 10 1 120 10\n',
+            '2 2 -1 30 -1 1 130 -1\n'])
+        file.close()
+        os.remove(path)
+
+    def testSaveWithHeaders(self):
+        self.fillIndex()
+        self.index.setHeadersSize(10)
+        fd, path = tempfile.mkstemp()
+        ret =self.index.save(path, 0)
+        self.assertEquals(ret, True)
+        file = open(path, 'r')
+        lines = file.readlines()
+        self.assertEquals(lines,
+            ['FLUIDX1 #Flumotion\n',
+            'CHK POS LEN TS DUR KF TDT TDUR\n',
+            '0 10 1 10 10 1 110 10\n',
+            '1 11 1 20 10 1 120 10\n',
+            '2 12 -1 30 -1 1 130 -1\n'])
+        file.close()
+        os.remove(path)
+
+    def testSaveVoidIndex(self):
+        fd, path = tempfile.mkstemp()
+        self.failUnlessRaisesWithMessage("The index doesn't contain any "
+            "entry and it will not be saved", self.index.save, path)
+        os.remove(path)
+
+    def testSaveBadFile(self):
+        self.fillIndex()
+        self.failUnlessRaisesWithMessage("Failed to open output file ",
+            self.index.save, '/')
+
+    def testLoadIndex(self):
+        fd, path = tempfile.mkstemp(suffix='.index')
+        tmpFile = open(path, 'w')
+        tmpFile.write(self.INDEX % ('FLUIDX1 #Flumotion', 'TDT', ' 120'))
+        tmpFile.flush()
+        self.index.loadIndexFile(path)
+        self.assertEquals(len(self.index._index), 3)
+        os.remove(path)
+
+    def testLoadIndexWithBadExtension(self):
+        fd, path = tempfile.mkstemp()
+        self.failUnlessRaisesWithMessage("This file is not a valid index: "
+            "the extension of this file is not",
+            self.index.loadIndexFile, path)
+        os.remove(path)
+
+    def testLoadIndexWithBadPath(self):
+        self.failUnlessRaisesWithMessage("This file is not a valid index: "
+            "error reading index file",
+            self.index.loadIndexFile, "/bad/bad.index")
+
+    def testLoadIndexWithEmptyFile(self):
+        fd, path = tempfile.mkstemp(suffix='.index')
+        self.failUnlessRaisesWithMessage("This file is not a valid index: "
+            "the file is empty",
+            self.index.loadIndexFile, path)
+        os.remove(path)
+
+    def testLoadIndexWithBadHeaders(self):
+        fd, path = tempfile.mkstemp(suffix='.index')
+        tmpFile = open(path, 'w')
+        tmpFile.write(self.INDEX % ('FLIDX1 #Flumotion', 'TDT', ' 120'))
+        tmpFile.flush()
+        self.failUnlessRaisesWithMessage("This file is not a valid index: "
+            "header is not",
+            self.index.loadIndexFile, tmpFile.name)
+        tmpFile.close()
+        os.remove(path)
+
+    def testLoadIndexWithBadKeys(self):
+        fd, path = tempfile.mkstemp(suffix='.index')
+        tmpFile = open(path, 'w')
+        tmpFile.write(self.INDEX % ('FLUIDX1 #Flumotion', 'TD', ' 120'))
+        tmpFile.flush()
+        self.failUnlessRaisesWithMessage("This file is not a valid index: "
+            "keys definition is not",
+            self.index.loadIndexFile, tmpFile.name)
+        tmpFile.close()
+        os.remove(path)
+
+    def testLoadIndexWithBadEntry(self):
+        fd, path = tempfile.mkstemp(suffix='.index')
+        tmpFile = open(path, 'w')
+        tmpFile.write(self.INDEX % ('FLUIDX1 #Flumotion', 'TDT',
+                                    ' NotAParsableNumber'))
+        tmpFile.flush()
+        self.failUnlessRaisesWithMessage("This file is not a valid index: "
+            "could not parse one of the entries",
+            self.index.loadIndexFile, tmpFile.name)
+        tmpFile.close()
+        os.remove(path)
+
+    def testLoadIndexWithMissingEntry(self):
+        fd, path = tempfile.mkstemp(suffix='.index')
+        tmpFile = open(path, 'w')
+        tmpFile.write(self.INDEX % ('FLUIDX1 #Flumotion', 'TDT',
+                                    ''))
+        tmpFile.flush()
+        self.failUnlessRaisesWithMessage("This file is not a valid index: "
+            "one of the entries doesn't have enough",
+            self.index.loadIndexFile, tmpFile.name)
+        tmpFile.close()
+        os.remove(path)
+
+    def testClipTimestamp(self):
+
+        def checkClipResults(entries, length, start, stop):
+            self.assertEquals(len(entries), length)
+            self.assertEquals(entries[0]['timestamp'], start)
+            self.assertEquals(entries[-1]['timestamp'], stop)
+
+        self.fillBigIndex()
+        # test lower boundary
+        entries = self.index.clipTimestamp(0, 195)
+        checkClipResults(entries, 20, 0, 190)
+        # test higher boundary
+        entries = self.index.clipTimestamp(1, 200)
+        checkClipResults(entries, 21, 0, 200)
+        # test inside boundaries
+        entries = self.index.clipTimestamp(10, 195)
+        checkClipResults(entries, 19, 10, 190)
+        # test start outside lower boundary
+        entries = self.index.clipTimestamp(-10, 195)
+        checkClipResults(entries, 20, 0, 190)
+        # test stop outside highest boundary
+        entries = self.index.clipTimestamp(900, 1001)
+        checkClipResults(entries, 9, 900, 980)
+        self.assertEquals(len(entries), 9)
+        # test all outside lower boundary
+        entries = self.index.clipTimestamp(-10, -12)
+        self.assertEquals(entries, None)
+        # test all outside highest boundary
+        entries = self.index.clipTimestamp(1001, 1100)
+        self.assertEquals(entries, None)
