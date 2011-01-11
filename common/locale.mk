e504f6baf852066b47c96b74d9f7f1c78313a841
Andoni Morales Alastruey <amorales@flumotion.com>
2011-01-11 15:49:27 +0100

    Remove common folder because it's now a submodule of flumotion-common

--- common/locale.mk
+++
@@ -1,3 +0,0 @@
-$(PROJECT).locale.xml: $(top_srcdir)/po/LINGUAS $(top_srcdir)/common/gen-locale-xml.py
-	$(PYTHON) $(top_srcdir)/common/gen-locale-xml.py \
-                $(GETTEXT_PACKAGE) $(PROJECT) `cat $(top_srcdir)/po/LINGUAS` > $@
--- common/rpm.mk
+++
@@ -1,8 +0,0 @@
-rpm: dist
-	rm -f *.rpm
-	for i in RPMS SRPMS SOURCES SPECS BUILD; do mkdir -p rpmbuild/$$i; done
-	cp flumotion-*.tar.bz2 rpmbuild/SOURCES
-	rpmbuild --nodeps --define "_topdir `pwd`/rpmbuild" -ba $(PACKAGE).spec
-	mv rpmbuild/RPMS/*/flumotion*.rpm .
-	mv rpmbuild/SRPMS/flumotion*.rpm .
-	rm -rf rpmbuild
