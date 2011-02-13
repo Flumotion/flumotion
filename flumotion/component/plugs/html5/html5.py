dd2ada41500f88574a22886596876af5f38c37a8
Zaheer Abbas Merali <zaheerabbas@merali.org>
2011-02-13 13:01:45 +0000

    First pass of html5 web page serving.

---
+++ flumotion/component/plugs/html5/html5.py
@@ -0,0 +1,128 @@
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+#
+# Flumotion - a streaming media server
+# Copyright (C) 2008 Fluendo, S.L. (www.fluendo.com).
+# All rights reserved.
+
+# This file may be distributed and/or modified under the terms of
+# the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+# This file is distributed without any warranty; without even the implied
+# warranty of merchantability or fitness for a particular purpose.
+# See "LICENSE.GPL" in the source distribution for more information.
+
+# Licensees having purchased or holding a valid Flumotion Advanced
+# Streaming Server license may use this file in accordance with the
+# Flumotion Advanced Streaming Server Commercial License Agreement.
+# See "LICENSE.Flumotion" in the source distribution for more information.
+
+# Headers in this file shall remain intact.
+
+from twisted.web.resource import Resource
+from twisted.web.static import Data
+
+from flumotion.common import log
+from flumotion.common.errors import ComponentStartError
+from flumotion.component.misc.httpserver.httpserver import HTTPFileStreamer
+from flumotion.component.plugs.base import ComponentPlug
+
+__version__ = "$Rev$"
+
+HTML5TEMPLATE = \
+"""
+<html>
+<head><title>Flumotion Stream</title></head>
+<body>
+<video height="%(height)d" width="%(width)d">
+<source type="%(mime-type)s; codecs='%(codecs)s'" src="%(stream-url)s">
+</source>
+</video>
+</body>
+"""
+
+def _htmlbool(value):
+    if value:
+        return 'true'
+    return 'false'
+
+
+class Html5DirectoryResource(Resource):
+    """I generate the directory used to serve an html5 viewing page
+    It contains::
+    - a html file, usually called index.html.
+    """
+
+    def __init__(self, mount_point, properties):
+        Resource.__init__(self)
+
+        index_name = properties.get('index', 'index.html')
+
+        root = mount_point
+        if not root.endswith("/"):
+            root += "/"
+        if index_name != 'index.html':
+            root = None
+        self._mount_point_root = root
+        self._properties = properties
+        self._index_content = self._get_index_content()
+        self._index_name = index_name
+        self._addChildren()
+
+    def _addChildren(self):
+        self.putChild(self._index_name,
+                      self._index_content)
+        self.putChild('', self._index_content)
+
+    def _get_index_content(self):
+        ns = {}
+        for attribute in ['codecs',
+                          'mime-type',
+                          'width',
+                          'height',
+                          'stream-url']:
+            ns[attribute] = self._properties[attribute]
+
+        content = HTML5TEMPLATE % ns
+        return Data(content, 'text/html')
+
+
+class ComponentHtml5Plug(ComponentPlug):
+    """I am a component plug for a http-server which plugs in a
+    http resource containing a html5 viewing page.
+    """
+
+    def start(self, component):
+        """
+        @type component: L{HTTPFileStreamer}
+        """
+        if not isinstance(component, HTTPFileStreamer):
+            raise ComponentStartError(
+                "An HTML5Plug %s must be plugged into a "
+                " HTTPFileStreamer component, not a %s" % (
+                self, component.__class__.__name__))
+        log.debug('html5', 'Attaching to %r' % (component, ))
+        resource = Html5DirectoryResource(component.getMountPoint(),
+                                          self.args['properties'])
+        component.setRootResource(resource)
+
+
+def test():
+    import sys
+    from twisted.internet import reactor
+    from twisted.python.log import startLogging
+    from twisted.web.server import Site
+    startLogging(sys.stderr)
+
+    properties = {'width': 320,
+                  'height': 240,
+                  'stream-url': '/stream.ogg',
+                  'buffer-size': 40}
+    root = Html5DirectoryResource('/', properties)
+    site = Site(root)
+
+    reactor.listenTCP(8080, site)
+    reactor.run()
+
+if __name__ == "__main__":
+    test()
---
+++ flumotion/component/plugs/html5/wizard_gtk.py
@@ -0,0 +1,146 @@
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+#
+# Flumotion - a streaming media server
+# Copyright (C) 2004,2005,2006,2007,2008 Fluendo, S.L. (www.fluendo.com).
+# All rights reserved.
+
+# This file may be distributed and/or modified under the terms of
+# the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+# This file is distributed without any warranty; without even the implied
+# warranty of merchantability or fitness for a particular purpose.
+# See "LICENSE.GPL" in the source distribution for more information.
+
+# Licensees having purchased or holding a valid Flumotion Advanced
+# Streaming Server license may use this file in accordance with the
+# Flumotion Advanced Streaming Server Commercial License Agreement.
+# See "LICENSE.Flumotion" in the source distribution for more information.
+
+# Headers in this file shall remain intact.
+
+"""Wizard plugin for the html5 http plug
+"""
+
+import gettext
+from zope.interface import implements
+
+from flumotion.admin.assistant.interfaces import IHTTPConsumerPlugin, \
+        IHTTPConsumerPluginLine
+from flumotion.admin.assistant.models import HTTPServer, HTTPPlug
+from flumotion.ui.plugarea import WizardPlugLine
+
+_ = gettext.gettext
+
+__version__ = "$Rev$"
+
+# Copied from posixpath.py
+
+
+def slashjoin(a, *p):
+    """Join two or more pathname components, inserting '/' as needed"""
+    path = a
+    for b in p:
+        if b.startswith('/'):
+            path = b
+        elif path == '' or path.endswith('/'):
+            path += b
+        else:
+            path += '/' + b
+    return path
+
+
+class Html5HTTPPlug(HTTPPlug):
+    """I am a model representing the configuration file for a
+    HTML5 HTTP streaming plug.
+    """
+    plugType = "component-html5"
+
+    # Component
+
+    def getProperties(self):
+        p = super(Html5HTTPPlug, self).getProperties()
+        #TODO: find the encoders and muxer and pass in to the Html5HTTPPlug
+        p.codecs = "vorbis,vp8"
+        p.mime_type = "video/webm"
+        p.stream_url = self.streamer.getURL()
+
+        width = 320
+        height = 240
+        if self.videoProducer:
+            width = self.videoProducer.properties.width
+            height = self.videoProducer.properties.height
+
+        p.width = width
+        p.height = height
+
+        return p
+
+
+class Html5HTTPServer(HTTPServer):
+    """I am a model representing the configuration file for a
+    HTTP server component which will be used to serve an html5 
+    video watching page.
+    Most of the interesting logic here is actually in a plug.
+    """
+    componentType = 'http-server'
+
+    def __init__(self, streamer, audioProducer, videoProducer, mountPoint):
+        """
+        @param streamer: streamer
+        @type  streamer: L{HTTPStreamer}
+        @param audioProducer: audio producer
+        @type  audioProducer: L{flumotion.admin.assistant.models.AudioProducer}
+           subclass or None
+        @param videoProducer: video producer
+        @type  videoProducer: L{flumotion.admin.assistant.models.VideoProducer}
+           subclass or None
+        @param mountPoint:
+        @type  mountPoint:
+        """
+        self.streamer = streamer
+        super(Html5HTTPServer, self).__init__(mountPoint=mountPoint,
+                                                worker=streamer.worker)
+
+        porter = streamer.getPorter()
+        self.properties.porter_socket_path = porter.getSocketPath()
+        self.properties.porter_username = porter.getUsername()
+        self.properties.porter_password = porter.getPassword()
+        self.properties.port = porter.getPort()
+        self.properties.type = 'slave'
+        plug = Html5HTTPPlug(self, streamer, audioProducer, videoProducer)
+        self.addPlug(plug)
+
+    def getProperties(self):
+        properties = super(Html5HTTPServer, self).getProperties()
+        hostname = self.streamer.getHostname()
+        if hostname:
+            properties.hostname = hostname
+        return properties
+
+
+class Html5PlugLine(WizardPlugLine):
+    implements(IHTTPConsumerPluginLine)
+    gladeFile = ''
+
+    def __init__(self, wizard, description):
+        WizardPlugLine.__init__(self, wizard, None, description)
+        self.setActive(True)
+
+    def plugActiveChanged(self, active):
+        pass
+
+    def getConsumer(self, streamer, audioProducer, videoProducer):
+        mountPoint = slashjoin(streamer.properties.mount_point, "html5/")
+        return Html5HTTPServer(streamer, audioProducer,
+                                 videoProducer, mountPoint)
+
+
+class Html5WizardPlugin(object):
+    implements(IHTTPConsumerPlugin)
+
+    def __init__(self, wizard):
+        self.wizard = wizard
+
+    def getPlugWizard(self, description):
+        return Html5PlugLine(self.wizard, description)
