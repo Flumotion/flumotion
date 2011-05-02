68bdea59c36c3dca239e2b87a98a01f18f093567
Andoni Morales Alastruey <amorales@flumotion.com>
2011-05-02 14:59:39 +0200

    Add implementation and sink and source elements for the FGDP protocol

---
+++ flumotion/component/common/fgdp/fgdp.py
@@ -0,0 +1,334 @@
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+#
+# Flumotion - a streaming media server
+# Copyright (C) 2004,2005,2006,2007 Fluendo, S.L. (www.fluendo.com).
+# All rights reserved.
+
+# This file may be distributed and/or modified under the terms of
+# the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+# This file is distributed without any warranty; without even the implied
+# warranty of merchantability or fitness for a particular purpose.
+# See "LICENSE.GPL" in the source distribution for more information.
+
+# Licensees having purchased or holding a valid Flumotion Advanced
+# Streaming Server license may use this file in accordance with the
+# Flumotion Advanced Streaming Server Commercial License Agreement.
+# See "LICENSE.Flumotion" in the source distribution for more information.
+
+# Headers in this file shall remain intact.
+
+import gobject
+import gst
+
+from twisted.internet import reactor
+from twisted.python import failure
+
+from flumotion.component.common.fgdp import protocol as fgdp
+
+GDP_TYPE_PRODUCER = "producer-type"
+GDP_TYPE_CONSUMER = "consumer-type"
+
+
+class FDHandler():
+    """
+    Base class for elements handling file descriptors
+
+    @type  fdelement: L{gst.Element}
+    """
+
+    def __init__(self, fdelement):
+        self.fdelement = fdelement
+
+    ### FDHandler interface for subclasses
+
+    def connectFd(self, fd):
+        '''
+        Connect a file descriptor to the gstreamer element that will be
+        writting to it or reading from it
+
+        @type  fd: int
+        '''
+        raise NotImplemented("subclass must implement connectFD")
+
+    def disconnectFd(self, fd):
+        '''
+        Disconnects a file descriptor from the gstreamer element that is
+        writting to it or reading from it
+
+        @type  fd: int
+        '''
+        raise NotImplemented("subclass must implement disconnectFD")
+
+
+class FDSrc(FDHandler):
+    """
+    File descriptors handler based on fdsrc
+
+    @type  protocol: L{flummotion.common.gdp_protocol.FGDPBaseProtocol}
+    """
+
+    logCategory = 'gdp-producer'
+
+    protocol = None
+    _handler_id = None
+
+    def __init__(self, fdelement):
+        FDHandler.__init__(self, fdelement)
+
+    def _check_eos(self, pad, event):
+        if event.type == gst.EVENT_EOS:
+            # EOS are triggered after a disconnection, when the read in the
+            # socket is 0 Bytes. Remove the handler and close the connection
+            pad.remove_event_probe(self._handler_id)
+            if self.protocol is not None:
+                f = failure.Failure("Lost connection with client")
+                reactor.callFromThread(self.protocol.loseConnection)
+            return False
+        return True
+
+    def connectFd(self, fd):
+        # Unlock the state of the element, which should be already in the READY
+        # state. Add the fd and an event probe to detect disconnections
+        self.fdelement.set_locked_state(False)
+        self.fdelement.set_property('fd', fd)
+        srcpad = self.fdelement.get_pad("src")
+        self._handler_id = srcpad.add_event_probe(self._check_eos)
+        self.fdelement.set_state(gst.STATE_PLAYING)
+
+    def disconnectFd(self, _):
+        # Set back the element to the READY state, in which a fd can be
+        # added/changed and lock the state of the element
+        self.fdelement.set_state(gst.STATE_READY)
+        self.fdelement.get_state(0)
+        self.fdelement.set_locked_state(True)
+
+
+class MultiFDSink(FDHandler):
+    """
+    File descriptors handler based on fdsrc
+
+    @type  protocol: L{flummotion.common.gdp_protocol.FGDPBaseProtocol}
+    """
+
+    logCategory = 'gdp-consumer'
+
+    protocol = None
+    _activeFD = None
+    _handler_id = None
+
+    def __init__(self, fdelement):
+        FDHandler.__init__(self, fdelement)
+
+    def _on_client_removed(self, a, fd):
+        if self.protocol is not None:
+            f = failure.Failure("Lost connection with client")
+            reactor.callFromThread(self.protocol.loseConnection)
+        self.fdelement.handler_disconnect(self._handler_id)
+
+    def connectFd(self, fd):
+        self.fdelement.emit('add', fd)
+        self._activeFD = fd
+        self._handler_id = self.fdelement.connect('client-fd-removed',
+                                                  self._on_client_removed)
+
+    def disconnectFd(self, fd):
+        if self._activeFD == fd:
+            self.fdelement.emit('remove', fd)
+            self._activeFD = None
+
+
+class _ProtocolMixin():
+    """
+    Provides an abstraction for the start and stop of a client or server using
+    the FGDP protocol, which depends on the 'mode' selected amongst 'push' or
+    'pull'
+
+    @type  mode:     str
+    @type  host:     str
+    @type  port:     str
+    """
+
+    mode = ''
+    host = None
+    port = None
+    _listener = None
+
+    def start(self):
+        """
+        Starts a server/client using the FGDP protocol when the element
+        is ready.
+
+        """
+        if self.mode == 'push':
+            self._start_push()
+        else:
+            self._start_pull()
+
+    def stop(self):
+        """
+        Stops the server/client using the FGDP protocol.
+        """
+        if self._listener is not None:
+            self._listener.stopListening()
+
+    def _start_push(self):
+        self.info("Starting fgdp client")
+        factory = fgdp.FGDPClientFactory(self)
+        self._listener = reactor.connectTCP(self.host, self.port, factory)
+
+    def _start_pull(self):
+        self.info("Starting fgdp server")
+        factory = fgdp.FGDPServerFactory(self)
+        # FIXME: Handle port already in use
+        self._listener = reactor.listenTCP(self.port, factory)
+
+
+class FGDPBase(gst.Bin, _ProtocolMixin):
+    """
+    Base class for gstreamer elements using the FGDP protocol
+    """
+
+    mode = 'pull'
+    host = 'localhost'
+    port = 12000
+    username = 'user'
+    maxDelay = 5
+    version = '0.1'
+
+    __gproperties__ = {
+        'mode': (gobject.TYPE_STRING, 'mode',
+            "Connection mode: 'pull' or 'push'",
+            'pull', gobject.PARAM_READWRITE),
+        'host': (gobject.TYPE_STRING, 'host',
+            'Name of the host to connect (in push mode)',
+            'localhost', gobject.PARAM_READWRITE),
+        'port': (gobject.TYPE_INT, 'port',
+            'Connection port',
+            1, 10000, 100, gobject.PARAM_READWRITE),
+        'username': (gobject.TYPE_STRING, 'user name',
+            'Username for the authentication',
+            'user', gobject.PARAM_READWRITE),
+        'password': (gobject.TYPE_STRING, 'password',
+            'Password for the authentication',
+            'test', gobject.PARAM_READWRITE),
+        'version': (gobject.TYPE_STRING, 'version',
+            'Protocol version',
+            '0.1', gobject.PARAM_READWRITE),
+        'max-reconnection-delay': (gobject.TYPE_FLOAT,
+            'maximum delay between reconnections in seconds',
+            'Maximum delay between reconnections in seconds (for push mode)',
+            1, 100, 5, gobject.PARAM_READWRITE)}
+
+    __gsignals__ = {"connected": (gobject.SIGNAL_RUN_LAST,\
+                                  gobject.TYPE_NONE, []),
+                    "disconnected": (gobject.SIGNAL_RUN_LAST,\
+                                     gobject.TYPE_NONE,
+                                     (gobject.TYPE_STRING, ))}
+
+    def do_change_state(self, transition):
+        if transition == gst.STATE_CHANGE_READY_TO_PAUSED:
+            try:
+                self.prepare()
+                self.start()
+            except Exception, e:
+                self.error("Error changing state from READY to PAUSED: %s" % e)
+                self.stop()
+                return gst.STATE_CHANGE_FAILURE
+        elif transition == gst.STATE_CHANGE_PAUSED_TO_READY:
+            self.stop()
+        return gst.Bin.do_change_state(self, transition)
+
+    def do_set_property(self, prop, value):
+        if prop.name in ['mode', 'host', 'username', 'password', 'port',
+                         'version']:
+            setattr(self, prop.name, value)
+        elif prop.name == 'max-reconnection-delay':
+            self.maxDelay = float(value)
+        else:
+            raise AttributeError('unknown property %s' % prop.name)
+
+    def do_get_property(self, prop):
+        if prop.name in ['mode', 'host', 'username', 'password', 'port',
+                         'version']:
+            return getattr(self, prop.name)
+        if prop.name == 'max-reconnection-delay':
+            return self.maxDelay
+        raise AttributeError('unknown property %s' % prop.name)
+
+    def prepare(self):
+        """
+        Should be implemented by subclasses that needs to do something
+        befaore starting the server/client
+        """
+        pass
+
+
+class FGDPSink(FGDPBase, MultiFDSink):
+    '''
+    GStreamer sink element using the FGDP protocol
+    '''
+
+    mode = 'push'
+
+    __gstdetails__ = ('FGDPsink', 'Sink',
+                      'Flumotion GStreamer data protocol sink',
+                      'Flumotion DevTeam')
+
+    def __init__(self):
+        FGDPBase.__init__(self)
+        # Create elements
+        gdppay = gst.element_factory_make('gdppay')
+        self.fdelement = gst.element_factory_make('multifdsink')
+        # Set default properties
+        self.fdelement.set_property('sync', False)
+        self.fdelement.set_property('unit-type', 2)
+        self.fdelement.set_property('units-max', 500 * gst.MSECOND)
+        self.fdelement.set_property('units-soft-max', 400 * gst.MSECOND)
+        self.fdelement.set_property('recover-policy', 0)
+        # Create fd handler proxy
+        MultiFDSink.__init__(self, self.fdelement)
+        # Add elements to the bin and link them
+        self.add_many(gdppay, self.fdelement)
+        gdppay.link(self.fdelement)
+        # Create sink pads
+        self._sink_pad = gst.GhostPad('sink', gdppay.get_pad('sink'))
+        self.add_pad(self._sink_pad)
+
+
+class FGDPSrc(FGDPBase, FDSrc):
+    '''
+    GStreamer source element using the FGDP protocol
+    '''
+
+    mode = 'pull'
+
+    __gstdetails__ = ('FGDPsrc', 'Source',
+                      'Flumotion GStreamer data protocol source',
+                      'Flumotion DevTeam')
+
+    def __init__(self):
+        FGDPBase.__init__(self)
+        # Create elements
+        self.fdelement = gst.element_factory_make('fdsrc')
+        gdpdepay = gst.element_factory_make('gdpdepay')
+        # Add elements to the bin and link them
+        self.add_many(self.fdelement, gdpdepay)
+        self.fdelement.link(gdpdepay)
+        # Create fd handler proxy
+        FDSrc.__init__(self, self.fdelement)
+        # Create sink pads
+        self._src_pad = gst.GhostPad('src', gdpdepay.get_pad('src'))
+        self.add_pad(self._src_pad)
+
+    def prepare(self):
+        # Lock the state until we get the first connection and we can pass it
+        # a valid fd, otherwhise it will be using stdin.
+        self.fdelement.set_locked_state(True)
+
+
+gobject.type_register(FGDPSink)
+gst.element_register(FGDPSink, "fgdpsink", gst.RANK_MARGINAL)
+gobject.type_register(FGDPSrc)
+gst.element_register(FGDPSrc, "fgdpsrc", gst.RANK_MARGINAL)
---
+++ flumotion/component/common/fgdp/protocol.py
@@ -0,0 +1,490 @@
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+#
+# Flumotion - a streaming media server
+# Copyright (C) 2004,2005,2006,2007 Fluendo, S.L. (www.fluendo.com).
+# All rights reserved.
+
+# This file may be distributed and/or modified under the terms of
+# the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+# This file is distributed without any warranty; without even the implied
+# warranty of merchantability or fitness for a particular purpose.
+# See "LICENSE.GPL" in the source distribution for more information.
+
+# Licensees having purchased or holding a valid Flumotion Advanced
+# Streaming Server license may use this file in accordance with the
+# Flumotion Advanced Streaming Server Commercial License Agreement.
+# See "LICENSE.Flumotion" in the source distribution for more information.
+
+# Headers in this file shall remain intact.
+
+import base64
+
+from twisted.internet import reactor
+from twisted.internet.protocol import ReconnectingClientFactory, Factory
+from twisted.protocols.basic import LineReceiver
+
+from Crypto import Random
+from Crypto.Hash import SHA
+
+from flumotion.common import log
+
+__version__ = "$Rev$"
+
+PROTOCOL_NAME = "FGDP"
+
+
+class ProtocolException(Exception):
+    error = ''
+
+    def __init__(self, reason=''):
+        self.reason = reason
+
+    def __str__(self):
+        return "%s: %s" % (self.error, self.reason)
+
+
+class UnexpectedResponse(ProtocolException):
+    "Received an unexpected response, in a wrong state"
+    error = "Unexpected response"
+
+
+class UnexpectedCommand(ProtocolException):
+    "Received an unexpected command, in a wrong state"
+    error = "Unexpected command"
+
+
+class MalformedCommand(ProtocolException):
+    "The command is badly formed"
+    error = "Malformed command"
+
+
+class MalformedResponse(ProtocolException):
+    "The response is badly formed"
+    error = "Malformed response"
+
+
+class InvalidVersion(ProtocolException):
+    "The version of the protocol is not valid"
+    error = "Invalid version"
+
+
+class AuthenticationFailed(ProtocolException):
+    "Authentication is not valid and failed"
+    error = "Invalid authentication"
+
+
+class ErrorResponse(ProtocolException):
+    "Got an error response"
+    error = "Received error response"
+
+
+class Command():
+    '''
+    Command for the FGDP procotol, sent by the client to the server
+
+    @type command: str
+    @type content: content
+    @type version: tuple
+    '''
+
+    def __init__(self, command, content, version):
+        self.command = command
+        self.content = content
+        self.version = version
+
+    @staticmethod
+    def parseCommand(commandString, versionCheck=None):
+        '''
+        Parses a command string
+
+        @param commandString: string with the command
+        @type  commandString: str
+        @param versionCheck: version number to check
+        @type  versionCheck: tuple
+
+        @return: A newly created L{Command}
+        @rtype: L{Command}
+        @raise MalformedCommand: raised when the command is not valid
+        '''
+        try:
+            command, line = commandString.split(' ', 1)
+            content, protoVersion = line.rsplit(' ', 1)
+            protocol, versionStr = protoVersion.split('/')
+            version = tuple(map(int, versionStr.split('.')))
+        except ValueError:
+            raise MalformedCommand(commandString)
+        if protocol != PROTOCOL_NAME:
+            raise MalformedCommand(commandString)
+        if versionCheck and version != versionCheck:
+            raise InvalidVersion('version %s is not compatible with %s ' %
+                                 (version, versionCheck))
+        return Command(command, content, version)
+
+    def __str__(self):
+        return "%s %s %s/%s" % (self.command, self.content, PROTOCOL_NAME,
+                                '.'.join(map(str, self.version)))
+
+    def __eq__(self, obj):
+        if not isinstance(obj, Command):
+            return False
+        return (str(self) == str(obj))
+
+
+class Response():
+    '''
+    Response for the FGDP protocol, sent by the server to the client
+
+    @type command:  str
+    @type content:  str
+    @type version:  tuple
+    '''
+
+    def __init__(self, response, content, version):
+        self.response = response
+        self.content = content
+        self.version = version
+
+    @staticmethod
+    def parseResponse(responseString, versionCheck=None):
+        '''
+        Parses a response string
+
+        @param responseString: string with the response
+        @type  responseString: str
+        @param versionCheck: version number to check
+        @type  versionCheck: tuple
+
+        @return: A newly created L{Response}
+        @rtype: L{Response}
+        @raise MalformedResponse: raised when the command is not valid
+        '''
+        try:
+            protoVersion, line = responseString.split(' ', 1)
+            protocol, versionStr = protoVersion.split('/')
+            version = tuple(map(int, versionStr.split('.')))
+            response, content = line.split(' ', 1)
+        except ValueError:
+            raise MalformedResponse(responseString)
+        if protocol != PROTOCOL_NAME:
+            raise MalformedResponse(responseString)
+        if versionCheck and version != versionCheck:
+            raise InvalidVersion('version %s is not compatible with %s ' %
+                                 (version, versionCheck))
+        return Response(response, content, version)
+
+    def __str__(self):
+        return "%s/%s %s %s" % (PROTOCOL_NAME,
+                                '.'.join(map(str, self.version)),
+                                self.response, self.content)
+
+    def __eq__(self, obj):
+        if not isinstance(obj, Response):
+            return False
+        return (str(self) == str(obj))
+
+
+class FGDP_0_1():
+    '''
+    Definitions for the version 0.1 of the FGDP protocol.
+    Future extensions of the protocol should subclass this class.
+    '''
+
+    LOGIN_COMMAND = 'LOGIN'
+    AUTH_COMMAND = 'AUTH'
+
+    OK_RESPONSE = 'OK'
+    ERROR_RESPONSE = 'ERROR'
+    CHALLENGE_RESPONSE = 'CHALLENGE'
+
+
+class FGDPBaseProtocol(LineReceiver, log.Loggable):
+    """
+    Base class for the tiwsted side of the FGDP protocol
+    """
+
+    _transport = None
+    _gstElement = None
+    _fd = None
+    _user = ''
+    _password = ''
+
+    def __init__(self, gstElement):
+        self._gstElement = gstElement
+        self._gstElement.protocol = self
+
+    def startProtocol(self):
+        raise NotImplemented()
+
+    def stopProtocol(self):
+        raise NotImplemented()
+
+    def lineReceived(self, line):
+        raise NotImplemented()
+
+    def makeConnection(self, transport):
+        self._transport = transport
+        self._fd = transport.fileno()
+        self.connectionMade()
+
+    def connectionMade(self):
+        self.info("Connection made with peer, starting protocol")
+        self.startProtocol()
+
+    def connectionLost(self, reason):
+        self.info("Connection lost with FGDP peer: %s",
+                  reason.getErrorMessage())
+        self.stopProtocol()
+
+    def loseConnection(self):
+        if self._transport is not None:
+            self._transport.loseConnection()
+
+    def _makeHash(self, values):
+        sha = SHA.new()
+        sha.update(':'.join(values))
+        return sha.hexdigest()
+
+    def _sendMessage(self, message, transport=None):
+        transport = transport or self._transport
+        self.debug('Sending message: "%s"', message)
+        transport.write("%s\r\n" % message)
+
+    def _disconnectFD(self):
+        if self._fd != None:
+            self._gstElement.disconnectFd(self._fd)
+
+    def _delegateFD(self):
+        # Take out the fd from twisted reactor and pass it to element
+        # using it
+        # See http://twistedmatrix.com/trac/ticket/1796
+        reactor.removeReader(self._transport)
+        self._gstElement.connectFd(self._fd)
+
+
+class FGDPServer_0_1(FGDP_0_1, FGDPBaseProtocol):
+    '''
+    Implementation of the server-side FGDP protocol for version 0.1
+    '''
+
+    logCategory = 'fgdp-server'
+
+    SERVER_STATE_DISCONNECTED = "disconnected"
+    SERVER_STATE_AUTHENTICATE = "authenticate"
+    SERVER_STATE_CONNECTED = "connected"
+
+    _state = SERVER_STATE_DISCONNECTED
+    _version = (0, 1)
+    _challenge = ''
+    _random = None
+
+    def __init__(self, gstElement):
+        self._user = gstElement.username
+        self._password = gstElement.password
+        self._random = Random.new()
+        FGDPBaseProtocol.__init__(self, gstElement)
+
+    def makeConnection(self, transport):
+        self.factory.clients += 1
+        if self.factory.clients > 1:
+            r = Response(self.ERROR_RESPONSE, "already connected",
+                           self._version)
+            self._sendMessage(r, transport)
+            self.warning("Trying to make a new connection, but a client "
+                         "is already connected.")
+            transport.loseConnection()
+            return
+        FGDPBaseProtocol.makeConnection(self, transport)
+
+    def startProtocol(self):
+        pass
+
+    def stopProtocol(self):
+        self.info("Stopping protocol session")
+        self.connected = 0
+        self.factory.clients -= 1
+        self._state = self.SERVER_STATE_DISCONNECTED
+        self._disconnectFD()
+
+    def lineReceived(self, line):
+        # Parse command and check state
+        try:
+            command = Command.parseCommand(line)
+            self._checkState(command)
+        except (ErrorResponse, MalformedCommand, UnexpectedCommand), e:
+            self._handleError(e)
+            return
+        # State DISCONNECTED
+        if self._state == self.SERVER_STATE_DISCONNECTED:
+            self._user = command.content
+            self._challengeClient()
+        # State AUTHENTICATE
+        elif self._state == self.SERVER_STATE_AUTHENTICATE:
+            try:
+                self._checkAuthentication(command)
+            except AuthenticationFailed, e:
+                self._handleError(e)
+                return
+            self._startStreaming()
+
+    def _checkState(self, command):
+        if self._state == self.SERVER_STATE_DISCONNECTED and \
+                command.command != self.LOGIN_COMMAND:
+            raise UnexpectedCommand(command.command)
+        if self._state == self.SERVER_STATE_AUTHENTICATE and \
+                command.command != self.AUTH_COMMAND:
+            raise UnexpectedCommand(command.command)
+        if self._state == self.SERVER_STATE_CONNECTED:
+            # FIXME: Non fatal error
+            raise UnexpectedCommand(command.command)
+
+    def _handleError(self, error):
+        self.warning("%s", error)
+        response = Response(ErrorResponse, "Server error: %s" % error,
+                            self._version)
+        self._sendMessage(response)
+        self.loseConnection()
+
+    def _challengeClient(self):
+        self.info("Challenging client")
+        self._state = self.SERVER_STATE_AUTHENTICATE
+        self._challenge = base64.b64encode(self._random.read(1024))
+        response = Response(self.CHALLENGE_RESPONSE, self._challenge,
+                           self._version)
+        self._sendMessage(response)
+
+    def _startStreaming(self):
+        self._state = self.SERVER_STATE_CONNECTED
+        response = Response(self.OK_RESPONSE, 'Authenticated', self._version)
+        self._sendMessage(response)
+        self.info("Started streaming")
+        self._delegateFD()
+
+    def _checkAuthentication(self, command):
+        digest = self._makeHash([self._user, self._password, self._challenge])
+        if digest != command.content:
+            raise AuthenticationFailed(
+                    "could not verify the challenge response")
+        return False
+
+
+class FGDPClient_0_1(FGDP_0_1, FGDPBaseProtocol):
+    '''
+    Implementation of the client-side FGDP protocol for version 0.1
+    '''
+
+    logCategory = 'fgdp-client'
+
+    CLIENT_STATE_DISCONNECTED = "disconnected"
+    CLIENT_STATE_LOGIN = "login"
+    CLIENT_STATE_AUTHENTICATING = "authenticate"
+    CLIENT_STATE_CONNECTED = "connected"
+
+    _version = (0, 1)
+    _state = CLIENT_STATE_DISCONNECTED
+
+    def __init__(self, gstElement):
+        self._user = gstElement.username
+        self._password = gstElement.password
+        FGDPBaseProtocol.__init__(self, gstElement)
+
+    def startProtocol(self):
+        self.info("Starting protocol session")
+        self._login()
+
+    def stopProtocol(self):
+        self.info('Stopping protocol session')
+        self._state = self.CLIENT_STATE_DISCONNECTED
+        self._disconnectFD()
+
+    def lineReceived(self, line):
+        # Parse response and check state
+        try:
+            response = Response.parseResponse(line)
+            self._checkState(response)
+        except (MalformedResponse, ErrorResponse, UnexpectedResponse), e:
+            self.warning("%s", e)
+            self.loseConnection()
+            return
+        # State LOGIN
+        if self._state == self.CLIENT_STATE_LOGIN:
+            self._authenticate(response)
+        # State AUTHENTICATING
+        elif self._state == self.CLIENT_STATE_AUTHENTICATING:
+            self._startStreaming()
+
+    def _checkState(self, response):
+        if response.response == self.ERROR_RESPONSE:
+            raise ErrorResponse(response.content)
+        if self._state == self.CLIENT_STATE_LOGIN and \
+                response.response != self.CHALLENGE_RESPONSE:
+            raise UnexpectedResponse(response.content)
+        if self._state == self.CLIENT_STATE_AUTHENTICATING and \
+                response.response != self.OK_RESPONSE:
+            raise UnexpectedResponse(response.content)
+        if self._state == self.CLIENT_STATE_CONNECTED:
+            raise UnexpectedResponse(response.content)
+
+    def _login(self):
+        self.info('Starting client login with user=%s, password=%s',
+                  self._user, self._password)
+        self._state = self.CLIENT_STATE_LOGIN
+        command = Command(self.LOGIN_COMMAND, self._user, self._version)
+        self._sendMessage(command)
+
+    def _authenticate(self, response):
+        self.info('Authenticating user with challenge %s', response.content)
+        self._state = self.CLIENT_STATE_AUTHENTICATING
+        res = self._makeHash([self._user, self._password, response.content])
+        command = Command(self.AUTH_COMMAND, res, self._version)
+        self._sendMessage(command)
+
+    def _startStreaming(self):
+        self.info("Starting streaming")
+        self._state = self.CLIENT_STATE_CONNECTED
+        self._delegateFD()
+
+
+class FGDPClientFactory(ReconnectingClientFactory, log.Loggable):
+    logCategory = 'fgdp-client'
+
+    _supportedVersions = ['0.1']
+
+    def __init__(self, gstElement):
+        self.gstElement = gstElement
+        self._setProtocol(gstElement.version)
+
+    def _setProtocol(self, version):
+        if version in self._supportedVersions:
+            classname = 'FGDPClient_%s' % version.replace('.', '_')
+            self.protocol = globals()[classname]
+
+    def buildProtocol(self, addr):
+        p = self.protocol(self.gstElement)
+        p.factory = self
+        return p
+
+    def retry(self, connector=None):
+        self.info("Trying reconnection with FGDP peer")
+        return ReconnectingClientFactory.retry(self, connector)
+
+
+class FGDPServerFactory(Factory):
+
+    clients = 0
+    _supportedVersions = ['0.1']
+
+    def __init__(self, gstElement):
+        self.gstElement = gstElement
+        self._setProtocol(gstElement.version)
+
+    def _setProtocol(self, version):
+        if version in self._supportedVersions:
+            classname = 'FGDPServer_%s' % version.replace('.', '_')
+            self.protocol = globals()[classname]
+
+    def buildProtocol(self, addr):
+        p = self.protocol(self.gstElement)
+        p.factory = self
+        return p
