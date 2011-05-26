Flumotion/flumotion

commit e447e7fdc5f68a7fead8f71a261da1df82f88741

Add API to handle 'flumotion-reset' events

flumotion/component/decodercomponent.py
@@ -27,7 +27,7 @@ import gst
 import gst.interfaces
 
 from flumotion.common.i18n import N_, gettexter
-from flumotion.common import errors, messages
+from flumotion.common import errors, messages, gstreamer
 from flumotion.component.effects.audiorate import audiorate
 from flumotion.component.effects.videorate import videorate
 from flumotion.component.effects.videoscale import videoscale
@@ -64,7 +64,7 @@ class DecoderComponent(fc.ReconfigurableComponent):
     def _depay_reset_event(self, pad, event, eater):
         if event.type != gst.EVENT_CUSTOM_DOWNSTREAM:
             return True
-        if event.get_structure().get_name() != 'flumotion-reset':
+        if gstreamer.event_is_flumotion_reset(event):
             return True
         self.info("Received flumotion-reset, not droping buffers anymore")
 

flumotion/component/feedcomponent.py
@@ -733,7 +733,7 @@ class ReconfigurableComponent(ParseLaunchComponent):
         def input_reset_event(pad, event):
             if event.type != gst.EVENT_CUSTOM_DOWNSTREAM:
                 return True
-            if event.get_structure().get_name() != 'flumotion-reset':
+            if gstreamer.event_is_flumotion_reset(event):
                 return True
             if self.disconnectedPads:
                 return False
@@ -794,12 +794,9 @@ class ReconfigurableComponent(ParseLaunchComponent):
             pad.set_blocked_async(False, self._on_eater_blocked)
 
     def _send_reset_event(self):
-        event = gst.event_new_custom(gst.EVENT_CUSTOM_DOWNSTREAM,
-                                     gst.Structure('flumotion-reset'))
-
         for elem in self.get_output_elements():
             pad = elem.get_pad('sink')
-            pad.send_event(event)
+            pad.send_event(gstreamer.flumotion_reset_event())
 
     def _unlink_pads(self, element, directions):
         for pad in element.pads():

flumotion/common/gstreamer.py
@@ -166,6 +166,17 @@ def get_state_change(old, new):
     return table.get((old, new), 0)
 
 
+def flumotion_reset_event():
+    ''' Helper method to create a 'flumotion-reset' event '''
+    return gst.event_new_custom(gst.EVENT_CUSTOM_DOWNSTREAM,
+                                gst.Structure('flumotion-reset'))
+
+
+def event_is_flumotion_reset(event):
+    ''' Helper method to check if an event is a 'flumotion-reset' event '''
+    return event.get_structure().get_name() == 'flumotion-reset'
+
+
 class StateChangeMonitor(dict, log.Loggable):
 
     def __init__(self):

flumotion/component/decoders/generic/generic.py
@@ -24,7 +24,7 @@ import gobject
 import threading
 
 from flumotion.component import decodercomponent as dc
-from flumotion.common import messages
+from flumotion.common import messages, gstreamer
 from flumotion.common.i18n import N_, gettexter
 
 T_ = gettexter()
@@ -163,7 +163,7 @@ class SyncKeeper(gst.Element):
             if event.type == gst.EVENT_NEWSEGMENT:
                 u, r, f, start, s, position = event.parse_new_segment()
                 self._update_sync_point(start, position)
-            if event.get_structure().get_name() == 'flumotion-reset':
+            if gstreamer.event_is_flumotion_reset(event):
                 self._resetReceived = True
                 self._send_new_segment = True
         finally:

flumotion/component/effects/videoscale/videoscale.py
@@ -102,9 +102,7 @@ class VideoscaleBin(gst.Bin):
             # unlink the sink and source pad of the old deinterlacer
             reactor.callFromThread(blockPad.set_blocked, False)
 
-        event = gst.event_new_custom(gst.EVENT_CUSTOM_DOWNSTREAM,
-            gst.Structure('flumotion-reset'))
-        self._sinkPad.send_event(event)
+        self._sinkPad.send_event(gstreamer.flumotion_reset_event())
 
         # We might be called from the streaming thread
         self.info("Replaced capsfilter")

commit 9e7df3ea1c56e65e836fa9817004788bd6246947

Fix thinko introduced in e447e7fdc

flumotion/component/decodercomponent.py
@@ -64,7 +64,7 @@ class DecoderComponent(fc.ReconfigurableComponent):
     def _depay_reset_event(self, pad, event, eater):
         if event.type != gst.EVENT_CUSTOM_DOWNSTREAM:
             return True
-        if gstreamer.event_is_flumotion_reset(event):
+        if not gstreamer.event_is_flumotion_reset(event):
             return True
         self.info("Received flumotion-reset, not droping buffers anymore")
 

flumotion/component/feedcomponent.py
@@ -733,7 +733,7 @@ class ReconfigurableComponent(ParseLaunchComponent):
         def input_reset_event(pad, event):
             if event.type != gst.EVENT_CUSTOM_DOWNSTREAM:
                 return True
-            if gstreamer.event_is_flumotion_reset(event):
+            if not gstreamer.event_is_flumotion_reset(event):
                 return True
             if self.disconnectedPads:
                 return False